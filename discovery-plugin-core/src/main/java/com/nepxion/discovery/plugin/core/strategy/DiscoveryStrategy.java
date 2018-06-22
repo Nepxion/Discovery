eb6f39f800ce8bb28dfa38cf80b06982260fb36d
Nepxion <1394997@qq.com>
2018-06-23 00:05:33 +0800

    重构类结构

--- discovery-plugin-core/src/main/java/com/nepxion/discovery/plugin/core/strategy/DiscoveryStrategy.java
+++ discovery-plugin-core/src/main/java/com/nepxion/discovery/plugin/core/strategy/DiscoveryStrategy.java
@@ -16,8 +16,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import org.apache.commons.collections.MapUtils;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
