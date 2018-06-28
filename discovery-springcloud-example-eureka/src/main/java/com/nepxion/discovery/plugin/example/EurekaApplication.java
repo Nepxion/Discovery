c693ad8a509a7dd1eda3723ee41b935bc8dc2b67
Nepxion <1394997@qq.com>
2018-06-28 22:52:30 +0800

    增加Eureka Server

---
+++ discovery-springcloud-example-eureka/src/main/java/com/nepxion/discovery/plugin/example/EurekaApplication.java
@@ -0,0 +1,22 @@
+package com.nepxion.discovery.plugin.example;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+    }
+}
