4229702859c4e9458ca05444554a1cc2f41180a9
Nepxion <1394997@qq.com>
2018-06-28 14:29:51 +0800

    测试用例，增加Route接口

--- discovery-springcloud-example-a/src/main/java/com/nepxion/discovery/plugin/example/controller/DiscoveryController.java
+++ discovery-springcloud-example-a/src/main/java/com/nepxion/discovery/plugin/example/controller/DiscoveryController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
