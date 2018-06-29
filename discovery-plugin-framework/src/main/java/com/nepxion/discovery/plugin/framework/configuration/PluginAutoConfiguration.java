346dd5d518403f481f0d4b9d3fe628513b183194
Nepxion <1394997@qq.com>
2018-06-30 01:27:24 +0800

    升级版本

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/configuration/PluginAutoConfiguration.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/configuration/PluginAutoConfiguration.java
@@ -36,7 +36,7 @@ public class PluginAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery  v3.0.0");
+        System.out.println("Nepxion Discovery  v3.0.1");
         System.out.println("");
     }
 
