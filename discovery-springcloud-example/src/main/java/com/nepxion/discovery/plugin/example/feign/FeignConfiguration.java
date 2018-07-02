0000ecdbd0a46fe334ac324e951214f004a99cb8
Nepxion <1394997@qq.com>
2018-07-03 06:26:00 +0800

    添加Feign配置

--- discovery-springcloud-example/src/main/java/com/nepxion/discovery/plugin/example/feign/FeignConfiguration.java
+++ discovery-springcloud-example/src/main/java/com/nepxion/discovery/plugin/example/feign/FeignConfiguration.java
@@ -9,13 +9,31 @@ package com.nepxion.discovery.plugin.example.feign;
  * @version 1.0
  */
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfiguration {
-    @Bean
-    public FeignBeanFactoryPostProcessor feignBeanFactoryPostProcessor() {
-        return new FeignBeanFactoryPostProcessor();
+    // 该Issue只在Eureka下才会出现
+    @ConditionalOnClass(name = "org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry")
+    protected static class FeignBeanFactoryPostProcessorConfiguration {
+        @Bean
+        public BeanFactoryPostProcessor feignBeanFactoryPostProcessor() {
+            return new FeignBeanFactoryPostProcessor();
+        }
+    }
+
+    // 参考：https://github.com/spring-cloud/spring-cloud-netflix/issues/1952, https://github.com/spring-cloud/spring-cloud-netflix/issues/1064
+    protected static class FeignBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            BeanDefinition definition = beanFactory.getBeanDefinition("feignContext");
+            definition.setDependsOn("eurekaServiceRegistry", "inetUtils");
+        }
     }
 }
