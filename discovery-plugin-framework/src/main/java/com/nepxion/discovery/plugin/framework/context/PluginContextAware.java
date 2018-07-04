e45482d4551dd1a7ac346e638a91d8669f1a88bf
Nepxion <1394997@qq.com>
2018-07-04 14:51:22 +0800

    重构PluginContextAware

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginContextAware.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginContextAware.java
@@ -9,18 +9,79 @@ package com.nepxion.discovery.plugin.framework.context;
  * @version 1.0
  */
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.ResolvableType;
 import org.springframework.core.env.Environment;
 
 import com.nepxion.discovery.plugin.framework.constant.PluginConstant;
 
 public class PluginContextAware implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
     private Environment environment;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
         this.environment = applicationContext.getEnvironment();
+    }
+
+    public Object getBean(String name) throws BeansException {
+        return applicationContext.getBean(name);
+    }
+
+    public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+        return applicationContext.getBean(name, requiredType);
+    }
+
+    public Object getBean(String name, Object... args) throws BeansException {
+        return applicationContext.getBean(name, args);
+    }
+
+    public <T> T getBean(Class<T> requiredType) throws BeansException {
+        return applicationContext.getBean(requiredType);
+    }
+
+    public <T> T getBean(Class<T> requiredType, Object... args) throws BeansException {
+        return applicationContext.getBean(requiredType, args);
+    }
+
+    public boolean containsBean(String name) {
+        return applicationContext.containsBean(name);
+    }
+
+    public boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.isSingleton(name);
+    }
+
+    public boolean isPrototype(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.isPrototype(name);
+    }
+
+    public boolean isTypeMatch(String name, ResolvableType typeToMatch) throws NoSuchBeanDefinitionException {
+        return applicationContext.isTypeMatch(name, typeToMatch);
+    }
+
+    public boolean isTypeMatch(String name, Class<?> typeToMatch) throws NoSuchBeanDefinitionException {
+        return applicationContext.isTypeMatch(name, typeToMatch);
+    }
+
+    public Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+        return applicationContext.getType(name);
+    }
+
+    public String[] getAliases(String name) {
+        return applicationContext.getAliases(name);
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
     }
 
     public Boolean isRegisterControlEnabled() {
