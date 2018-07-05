4723406aa3609d863240f2dc55ef278762163675
Nepxion <1394997@qq.com>
2018-07-05 18:01:10 +0800

    升级版本

--- discovery-plugin-framework-eureka/src/main/java/com/nepxion/discovery/plugin/framework/configuration/EurekaAutoConfiguration.java
+++ discovery-plugin-framework-eureka/src/main/java/com/nepxion/discovery/plugin/framework/configuration/EurekaAutoConfiguration.java
@@ -29,7 +29,7 @@ public class EurekaAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery - Eureka Plugin  v3.3.4");
+        System.out.println("Nepxion Discovery - Eureka Plugin  v3.3.5");
         System.out.println("");
     }
 
--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DnewVersion=3.3.4
+call mvn versions:set -DnewVersion=3.3.5
 
 pause
