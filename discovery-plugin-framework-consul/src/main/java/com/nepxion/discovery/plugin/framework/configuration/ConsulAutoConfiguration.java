82ee86ffad10c07ffb1b2f7b03cfbde3fa7d7b2d
Nepxion <1394997@qq.com>
2018-07-05 21:56:23 +0800

    升级版本

--- discovery-plugin-framework-consul/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ConsulAutoConfiguration.java
+++ discovery-plugin-framework-consul/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ConsulAutoConfiguration.java
@@ -29,7 +29,7 @@ public class ConsulAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery - Consul Plugin  v3.3.6");
+        System.out.println("Nepxion Discovery - Consul Plugin  v3.3.7");
         System.out.println("");
     }
 
