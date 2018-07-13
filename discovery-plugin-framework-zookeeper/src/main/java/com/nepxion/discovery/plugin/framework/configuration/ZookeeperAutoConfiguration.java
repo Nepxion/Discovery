612aa2d84ff51939fa870b7d37a538e10c2ee31b
Nepxion <1394997@qq.com>
2018-07-13 12:05:59 +0800

    升级版本

--- discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ZookeeperAutoConfiguration.java
+++ discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ZookeeperAutoConfiguration.java
@@ -29,7 +29,7 @@ public class ZookeeperAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery - Zookeeper Plugin  v3.3.18");
+        System.out.println("Nepxion Discovery - Zookeeper Plugin  v3.3.19");
         System.out.println("");
     }
 
