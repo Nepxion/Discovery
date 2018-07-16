64f186ee9d8f820d9614034cafe84b1e15e2c7a0
Nepxion <1394997@qq.com>
2018-07-16 17:50:35 +0800

    增加JacksonSerializer

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/constant/PluginConstant.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/constant/PluginConstant.java
@@ -24,6 +24,7 @@ public class PluginConstant {
     public static final String RULE = "rule";
     public static final String REACH_MAX_LIMITED_COUNT = "reach max limited count";
 
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
     public static final String ENCODING_UTF_8 = "UTF-8";
     public static final String SEPARATE = ";";
 }
