1ec065cd9759dfbe740f93c267938cf5e8d7680d
Nepxion <1394997@qq.com>
2018-07-16 23:08:25 +0800

    增加动态版本功能

---
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/event/RuleClearedEvent.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.plugin.framework.event;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.io.Serializable;
+
+public class RuleClearedEvent implements Serializable {
+    private static final long serialVersionUID = -4942710381954711909L;
+}
