f36a55b512a5346ee9dc048271d1ce8a1abcb9fc
Nepxion <1394997@qq.com>
2018-07-24 01:07:35 +0800

    提交Spring Cloud Api Gateway示例

---
+++ discovery-springcloud-example/src/main/java/com/nepxion/discovery/plugin/example/DiscoveryApplicationGateway.java
@@ -0,0 +1,35 @@
+package com.nepxion.discovery.plugin.example;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DiscoveryApplicationGateway {
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "gateway");
+
+        new SpringApplicationBuilder(DiscoveryApplicationGateway.class).run(args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/iscovery-springcloud-example-a/**").uri("lb://discovery-springcloud-example-a"))
+                .build();
+    }
+}
