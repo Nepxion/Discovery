ffdfb80196bcac994cd15b833cc5b7e9a79366cc
Nepxion <1394997@qq.com>
2018-07-29 10:50:28 +0800

    增加Redis为远程配置中心

---
+++ discovery-console-extension-redis/src/main/java/com/nepxion/discovery/console/extension/redis/configuration/RedisConfigAutoConfiguration.java
@@ -0,0 +1,24 @@
+package com.nepxion.discovery.console.extension.redis.configuration;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.nepxion.discovery.console.extension.redis.adapter.RedisConfigAdapter;
+import com.nepxion.discovery.console.remote.ConfigAdapter;
+
+@Configuration
+public class RedisConfigAutoConfiguration {
+    @Bean
+    public ConfigAdapter configAdapter() {
+        return new RedisConfigAdapter();
+    }
+}
