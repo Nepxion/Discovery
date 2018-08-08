9420dae25a7c33daf8ab8f369c1f3f6ab61cf002
Nepxion <1394997@qq.com>
2018-08-09 00:11:15 +0800

    增加全局监听

--- discovery-plugin-config-center-extension-nacos/src/main/java/com/nepxion/discovery/plugin/configcenter/extension/nacos/adapter/NacosConfigAdapter.java
+++ discovery-plugin-config-center-extension-nacos/src/main/java/com/nepxion/discovery/plugin/configcenter/extension/nacos/adapter/NacosConfigAdapter.java
@@ -39,29 +39,52 @@ public class NacosConfigAdapter extends ConfigAdapter {
 
     @Override
     public String getConfig() throws Exception {
+        String config = getConfig(true);
+        if (StringUtils.isNotEmpty(config)) {
+            return config;
+        } else {
+            LOG.info("No global config is retrieved from Nacos server");
+        }
+
+        config = getConfig(false);
+        if (StringUtils.isNotEmpty(config)) {
+            return config;
+        } else {
+            LOG.info("No partial config is retrieved from Nacos server");
+        }
+
+        return null;
+    }
+
+    private String getConfig(boolean globalConfig) throws Exception {
         String groupKey = pluginContextAware.getGroupKey();
         String group = pluginAdapter.getGroup();
         String serviceId = pluginAdapter.getServiceId();
 
-        LOG.info("Get config from Nacos server, {}={}, serviceId={}", groupKey, group, serviceId);
+        LOG.info("Get config from Nacos server, {}={}, serviceId={}, globalConfig={}", groupKey, group, serviceId, globalConfig);
 
-        return nacosOperation.getConfig(group, serviceId);
+        return nacosOperation.getConfig(group, globalConfig ? group : serviceId);
     }
 
     @PostConstruct
     public void subscribeConfig() {
+        subscribeConfig(true);
+        subscribeConfig(false);
+    }
+
+    private void subscribeConfig(boolean globalConfig) {
         String groupKey = pluginContextAware.getGroupKey();
         String group = pluginAdapter.getGroup();
         String serviceId = pluginAdapter.getServiceId();
 
-        LOG.info("Subscribe config from Nacos server, {}={}, serviceId={}", groupKey, group, serviceId);
+        LOG.info("Subscribe config from Nacos server, {}={}, serviceId={}, globalConfig={}", groupKey, group, serviceId, globalConfig);
 
         try {
-            nacosOperation.subscribeConfig(group, serviceId, new NacosSubscribeCallback() {
+            nacosOperation.subscribeConfig(group, globalConfig ? group : serviceId, new NacosSubscribeCallback() {
                 @Override
                 public void callback(String config) {
                     if (StringUtils.isNotEmpty(config)) {
-                        LOG.info("Get config updated event from Nacos server, {}={}, serviceId={}", groupKey, group, serviceId);
+                        LOG.info("Get config updated event from Nacos server, {}={}, serviceId={}, globalConfig={}", groupKey, group, serviceId, globalConfig);
 
                         RuleEntity ruleEntity = pluginAdapter.getRule();
                         String rule = null;
@@ -71,17 +94,17 @@ public class NacosConfigAdapter extends ConfigAdapter {
                         if (!StringUtils.equals(rule, config)) {
                             fireRuleUpdated(new RuleUpdatedEvent(config), true);
                         } else {
-                            LOG.info("Retrieved config is same as current config, ignore to update, {}={}, serviceId={}", groupKey, group, serviceId);
+                            LOG.info("Retrieved config is same as current config, ignore to update, {}={}, serviceId={}, globalConfig={}", groupKey, group, serviceId, globalConfig);
                         }
                     } else {
-                        LOG.info("Get config cleared event from Nacos server, {}={}, serviceId={}", groupKey, group, serviceId);
+                        LOG.info("Get config cleared event from Nacos server, {}={}, serviceId={}, globalConfig={}", groupKey, group, serviceId, globalConfig);
 
                         fireRuleCleared(new RuleClearedEvent(), true);
                     }
                 }
             });
         } catch (Exception e) {
-            LOG.error("Subscribe config failed", e);
+            LOG.error("Subscribe config from Nacos server failed, " + groupKey + "=" + group + ", serviceId=" + serviceId + ", globalConfig=" + globalConfig, e);
         }
     }
 }
