e268cc6f9828f5a387a31eeb8aa454471251fc89
Nepxion <1394997@qq.com>
2018-08-23 09:57:26 +0800

    升级版本到3.6.17

--- discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ZookeeperAutoConfiguration.java
+++ discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/configuration/ZookeeperAutoConfiguration.java
@@ -30,7 +30,7 @@ public class ZookeeperAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery - " + ZookeeperConstant.DISCOVERY_PLUGIN + "  v3.6.16");
+        System.out.println("Nepxion Discovery - " + ZookeeperConstant.DISCOVERY_PLUGIN + "  v3.6.17");
         System.out.println("");
     }
 
--- version.bat
+++ version.bat
@@ -15,5 +15,5 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DnewVersion=3.6.16
+call mvn versions:set -DnewVersion=3.6.17
 pause
