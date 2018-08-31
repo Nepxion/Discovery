c32e635e4d8f20371a889ffe9b366c14e8819802
Nepxion <1394997@qq.com>
2018-08-31 14:22:10 +0800

    增加RestTemplateStrategyInterceptor机制

--- discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/DiscoveryApplicationA1.java
+++ discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/DiscoveryApplicationA1.java
@@ -9,17 +9,22 @@ package com.nepxion.discovery.plugin.example.service;
  * @version 1.0
  */
 
+import java.util.Collections;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import com.nepxion.discovery.plugin.example.service.impl.MyDiscoveryEnabledStrategy;
 import com.nepxion.discovery.plugin.example.service.impl.MyDiscoveryListener;
 import com.nepxion.discovery.plugin.example.service.impl.MyLoadBalanceListener;
 import com.nepxion.discovery.plugin.example.service.impl.MyRegisterListener;
 import com.nepxion.discovery.plugin.example.service.impl.MySubscriber;
+import com.nepxion.discovery.plugin.strategy.service.aop.RestTemplateStrategyInterceptor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -29,6 +34,15 @@ public class DiscoveryApplicationA1 {
         System.setProperty("spring.profiles.active", "a1");
 
         new SpringApplicationBuilder(DiscoveryApplicationA1.class).run(args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateStrategyInterceptor restTemplateStrategyInterceptor) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(restTemplateStrategyInterceptor));
+
+        return restTemplate;
     }
 
     @Bean
