15afbe283262807911143fd03b1678d66096faef
Nepxion <1394997@qq.com>
2018-08-31 14:22:08 +0800

    增加RestTemplateStrategyInterceptor机制

---
+++ discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/rest/ARestImpl.java
@@ -0,0 +1,41 @@
+package com.nepxion.discovery.plugin.example.service.rest;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+
+@RestController
+@ConditionalOnProperty(name = DiscoveryConstant.SPRING_APPLICATION_NAME, havingValue = "discovery-springcloud-example-a")
+public class ARestImpl extends AbstractRestImpl {
+    private static final Logger LOG = LoggerFactory.getLogger(ARestImpl.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(path = "/rest", method = RequestMethod.POST)
+    public String rest(@RequestBody String value) {
+        value = doRest(value);
+        value = restTemplate.postForEntity("http://discovery-springcloud-example-b/rest", value, String.class).getBody();  
+
+        LOG.info("调用路径：{}", value);
+
+        return value;
+    }
+}
---
+++ discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/rest/CRestImpl.java
@@ -0,0 +1,35 @@
+package com.nepxion.discovery.plugin.example.service.rest;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+
+@RestController
+@ConditionalOnProperty(name = DiscoveryConstant.SPRING_APPLICATION_NAME, havingValue = "discovery-springcloud-example-c")
+public class CRestImpl extends AbstractRestImpl {
+    private static final Logger LOG = LoggerFactory.getLogger(CRestImpl.class);
+
+    @RequestMapping(path = "/rest", method = RequestMethod.POST)
+    public String rest(@RequestBody String value) {
+        value = doRest(value);
+
+        LOG.info("调用路径：{}", value);
+
+        return value;
+    }
+}
