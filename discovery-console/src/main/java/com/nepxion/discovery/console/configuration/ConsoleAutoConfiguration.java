e0fb834c5a5691ab41f69c263cea8b51f193f5af
Nepxion <1394997@qq.com>
2018-09-03 12:45:02 +0800

    升级版本到4.7.7

--- discovery-console/src/main/java/com/nepxion/discovery/console/configuration/ConsoleAutoConfiguration.java
+++ discovery-console/src/main/java/com/nepxion/discovery/console/configuration/ConsoleAutoConfiguration.java
@@ -32,7 +32,7 @@ public class ConsoleAutoConfiguration {
         System.out.println("╚═══╩╩══╩══╩══╝╚╝╚══╩╝╚═╗╔╝");
         System.out.println("                      ╔═╝║");
         System.out.println("                      ╚══╝");
-        System.out.println("Nepxion Discovery - Console  v4.7.6");
+        System.out.println("Nepxion Discovery - Console  v4.7.7");
         System.out.println("");
     }
 
