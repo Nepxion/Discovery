06cbe6efb6b9132807f0e961f757ce945742af36
Nepxion <1394997@qq.com>
2018-09-09 16:35:03 +0800

    支持Apollo配置中间件

---
+++ discovery-common-apollo/src/main/java/com/nepxion/discovery/common/apollo/constant/ApolloConstant.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.common.apollo.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import com.ctrip.framework.apollo.core.ConfigConsts;
+
+public class ApolloConstant implements ConfigConsts {
+    public static final String NAMESPACE = "apollo.discovery.namespace";
+}
