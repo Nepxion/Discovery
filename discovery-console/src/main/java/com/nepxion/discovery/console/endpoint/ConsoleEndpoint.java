398e7c5b033537b252d731b6a88ac3277c7393e5
Nepxion <1394997@qq.com>
2018-09-15 14:24:36 +0800

    重构接口

--- discovery-console/src/main/java/com/nepxion/discovery/console/endpoint/ConsoleEndpoint.java
+++ discovery-console/src/main/java/com/nepxion/discovery/console/endpoint/ConsoleEndpoint.java
@@ -18,7 +18,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
