0e2e872eaaa064845e62d842aa35c9918bb7288b
Nepxion <1394997@qq.com>
2018-10-16 22:45:50 +0800

    增加作者

--- discovery-console-starter-redis/src/main/java/com/nepxion/discovery/console/redis/adapter/RedisConfigAdapter.java
+++ discovery-console-starter-redis/src/main/java/com/nepxion/discovery/console/redis/adapter/RedisConfigAdapter.java
@@ -6,6 +6,7 @@ package com.nepxion.discovery.console.redis.adapter;
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
+ * @author JiKai Sun
  * @version 1.0
  */
 
