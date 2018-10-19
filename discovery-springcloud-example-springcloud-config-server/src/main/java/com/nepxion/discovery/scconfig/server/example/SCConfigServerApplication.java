33a362a3b787809e2ce4c173f24450a5416865e7
Nepxion <1394997@qq.com>
2018-10-19 19:02:27 +0800

    增加@EnableDiscoveryClient

--- discovery-springcloud-example-springcloud-config-server/src/main/java/com/nepxion/discovery/scconfig/server/example/SCConfigServerApplication.java
+++ discovery-springcloud-example-springcloud-config-server/src/main/java/com/nepxion/discovery/scconfig/server/example/SCConfigServerApplication.java
@@ -12,9 +12,11 @@ package com.nepxion.discovery.scconfig.server.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class SCConfigServerApplication {
     public static void main(String[] args) {
