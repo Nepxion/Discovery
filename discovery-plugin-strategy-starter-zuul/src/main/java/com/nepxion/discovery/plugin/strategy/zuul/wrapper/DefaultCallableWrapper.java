a81d3548b47b12d61ebf6712cdef4af5585519f7
Nepxion <1394997@qq.com>
2018-10-31 17:56:31 +0800

    修正作者名

--- discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/wrapper/DefaultCallableWrapper.java
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/wrapper/DefaultCallableWrapper.java
@@ -6,7 +6,7 @@ package com.nepxion.discovery.plugin.strategy.zuul.wrapper;
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
- * @author Hao Wang
+ * @author Hao Huang
  * @version 1.0
  */
 
