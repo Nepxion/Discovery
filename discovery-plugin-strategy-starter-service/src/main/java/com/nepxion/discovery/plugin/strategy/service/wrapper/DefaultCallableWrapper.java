800196f83b8cd4ceae4429c8e9ee5d9cca11c983
Nepxion <1394997@qq.com>
2018-11-10 22:45:47 +0800

    服务端使用Hystrix做线程模式的服务隔离时，实现服务灰度路由的功能

---
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/wrapper/DefaultCallableWrapper.java
@@ -0,0 +1,39 @@
+package com.nepxion.discovery.plugin.strategy.service.wrapper;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @author Hao Huang
+ * @version 1.0
+ */
+
+import java.util.concurrent.Callable;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.nepxion.discovery.plugin.strategy.service.context.RestStrategyContext;
+import com.nepxion.discovery.plugin.strategy.wrapper.CallableWrapper;
+
+public class DefaultCallableWrapper implements CallableWrapper {
+    @Override
+    public <T> Callable<T> wrapCallable(Callable<T> delegate) {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+        return new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                try {
+                    RestStrategyContext.getCurrentContext().setRequestAttributes(requestAttributes);
+
+                    return delegate.call();
+                } finally {
+                    RestStrategyContext.clearCurrentContext();
+                }
+            }
+        };
+    }
+}
