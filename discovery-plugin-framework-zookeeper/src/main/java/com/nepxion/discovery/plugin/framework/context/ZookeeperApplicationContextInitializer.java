af55bcf21e3e811a36c885f7181a522108944d1c
Nepxion <1394997@qq.com>
2018-11-10 17:08:07 +0800

    增加外部元数据注入到远程配置中心的功能

--- discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/context/ZookeeperApplicationContextInitializer.java
+++ discovery-plugin-framework-zookeeper/src/main/java/com/nepxion/discovery/plugin/framework/context/ZookeeperApplicationContextInitializer.java
@@ -20,6 +20,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.plugin.framework.constant.ZookeeperConstant;
 import com.nepxion.discovery.plugin.framework.decorator.ZookeeperServiceRegistryDecorator;
+import com.nepxion.discovery.plugin.framework.util.MetadataUtil;
 
 public class ZookeeperApplicationContextInitializer extends PluginApplicationContextInitializer {
     @Override
@@ -41,6 +42,8 @@ public class ZookeeperApplicationContextInitializer extends PluginApplicationCon
             metadata.put(DiscoveryConstant.SPRING_APPLICATION_CONFIG_REST_CONTROL_ENABLED, PluginContextAware.isConfigRestControlEnabled(environment).toString());
             metadata.put(DiscoveryConstant.SPRING_APPLICATION_GROUP_KEY, PluginContextAware.getGroupKey(environment));
             metadata.put(DiscoveryConstant.SPRING_APPLICATION_CONTEXT_PATH, PluginContextAware.getContextPath(environment));
+
+            MetadataUtil.filter(metadata);
 
             return bean;
         } else {
