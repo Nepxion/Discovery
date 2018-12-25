41bc82bed3cf5e12c138c6e998e71ff25fb4ec0a
Nepxion <1394997@qq.com>
2018-12-25 16:59:33 +0800

    升级版本到3.8.0

--- discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
+++ discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
@@ -10,7 +10,7 @@ package com.nepxion.discovery.common.constant;
  */
 
 public class DiscoveryConstant {
-    public static final String DISCOVERY_VERSION = "3.8.0-RC2";
+    public static final String DISCOVERY_VERSION = "3.8.0";
 
     public static final String SPRING_APPLICATION_DISCOVERY_PLUGIN = "spring.application.discovery.plugin";
     public static final String SPRING_APPLICATION_REGISTER_CONTROL_ENABLED = "spring.application.register.control.enabled";
