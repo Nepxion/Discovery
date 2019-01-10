7b7502fa861dd4d33e2dde425dfe7d310222233a
yangfan <hyyangfan@gmail.com>
2019-01-10 21:50:39 +0800

    修复Hystrix获取Request的BUG

--- discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/context/ServiceStrategyContextHolder.java
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/context/ServiceStrategyContextHolder.java
@@ -5,30 +5,26 @@ package com.nepxion.discovery.plugin.strategy.service.context;
  * <p>Description: Nepxion Discovery</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
+ *
  * @author Haojun Ren
+ * @author Fan Yang
  * @version 1.0
  */
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.nepxion.discovery.plugin.strategy.constant.StrategyConstant;
+import java.util.Map;
 
 public class ServiceStrategyContextHolder {
-    @Autowired
-    private ConfigurableEnvironment environment;
 
     public ServletRequestAttributes getRestAttributes() {
-        Boolean hystrixThreadlocalSupported = environment.getProperty(StrategyConstant.SPRING_APPLICATION_STRATEGY_HYSTRIX_THREADLOCAL_SUPPORTED, Boolean.class, Boolean.FALSE);
-        if (hystrixThreadlocalSupported) {
-            return (ServletRequestAttributes) RestStrategyContext.getCurrentContext().getRequestAttributes();
-        } else {
-            return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        RequestAttributes requestAttributes = RestStrategyContext.getCurrentContext().getRequestAttributes();
+        if (requestAttributes == null) {
+            requestAttributes = RequestContextHolder.getRequestAttributes();
         }
+        return (ServletRequestAttributes) requestAttributes;
     }
 
     public Map<String, Object> getRpcAttributes() {
