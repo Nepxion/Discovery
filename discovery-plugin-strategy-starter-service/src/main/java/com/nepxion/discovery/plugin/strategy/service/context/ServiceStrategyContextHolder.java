649795d24c5e324eb888efdd0871f38b7cff6732
Nepxion <1394997@qq.com>
2019-01-12 12:51:21 +0800

    修复Hystrix获取Request的BUG

--- discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/context/ServiceStrategyContextHolder.java
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/context/ServiceStrategyContextHolder.java
@@ -5,25 +5,24 @@ package com.nepxion.discovery.plugin.strategy.service.context;
  * <p>Description: Nepxion Discovery</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
- *
  * @author Haojun Ren
  * @author Fan Yang
  * @version 1.0
  */
 
+import java.util.Map;
+
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.Map;
-
 public class ServiceStrategyContextHolder {
-
     public ServletRequestAttributes getRestAttributes() {
         RequestAttributes requestAttributes = RestStrategyContext.getCurrentContext().getRequestAttributes();
         if (requestAttributes == null) {
             requestAttributes = RequestContextHolder.getRequestAttributes();
         }
+
         return (ServletRequestAttributes) requestAttributes;
     }
 
--- discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyContextHolder.java
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyContextHolder.java
@@ -5,18 +5,16 @@ package com.nepxion.discovery.plugin.strategy.zuul.context;
  * <p>Description: Nepxion Discovery</p>
  * <p>Copyright: Copyright (c) 2017-2050</p>
  * <p>Company: Nepxion</p>
- *
  * @author Haojun Ren
  * @author Fan Yang
  * @version 1.0
  */
 
-import com.netflix.zuul.context.RequestContext;
-
 import javax.servlet.http.HttpServletRequest;
 
-public class ZuulStrategyContextHolder {
+import com.netflix.zuul.context.RequestContext;
 
+public class ZuulStrategyContextHolder {
     public HttpServletRequest getRequest() {
         HttpServletRequest request = ZuulStrategyContext.getCurrentContext().getRequest();
         if (request == null) {
