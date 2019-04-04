da409522920cd933b203bc7020f9ccd4e12d06ec
Nepxion <1394997@qq.com>
2019-04-04 18:39:35 +0800

    升级版本到3.8.5.1

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginApplicationContextInitializer.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginApplicationContextInitializer.java
@@ -11,7 +11,7 @@ package com.nepxion.discovery.plugin.framework.context;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -21,7 +21,7 @@ import com.nepxion.discovery.plugin.framework.decorator.DiscoveryClientDecorator
 public abstract class PluginApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        if (applicationContext instanceof AnnotationConfigServletWebServerApplicationContext) {
+        if (applicationContext instanceof AnnotationConfigEmbeddedWebApplicationContext) {
             applicationContext.getBeanFactory().addBeanPostProcessor(new InstantiationAwareBeanPostProcessorAdapter() {
                 @Override
                 public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
