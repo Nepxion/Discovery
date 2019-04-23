41ded51f3ff20a80f60d81f50a76921419a4f073
Nepxion <1394997@qq.com>
2019-04-23 18:56:57 +0800

    去掉Rule对象打印

--- discovery-plugin-config-center/src/main/java/com/nepxion/discovery/plugin/configcenter/parser/json/JsonConfigParser.java
+++ discovery-plugin-config-center/src/main/java/com/nepxion/discovery/plugin/configcenter/parser/json/JsonConfigParser.java
@@ -31,7 +31,7 @@ public class JsonConfigParser implements PluginConfigParser {
             RuleEntity ruleEntity = JsonUtil.fromJson(config, RuleEntity.class);
             ruleEntity.setContent(config);
 
-            LOG.info("Rule entity=\n{}", ruleEntity);
+            LOG.info("Rule content=\n{}", config);
 
             return ruleEntity;
         } catch (Exception e) {
