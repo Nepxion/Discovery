80dc44a3e2f3f8829cafcaf8902d4b55c70c61f6
Nepxion <1394997@qq.com>
2019-05-08 12:53:45 +0800

    增加从Zuul Filter获取Header的方式

--- discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyZuulFilter.java
+++ discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyZuulFilter.java
@@ -9,6 +9,9 @@ package com.nepxion.discovery.plugin.example.zuul.impl;
  * @version 1.0
  */
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
@@ -31,7 +34,11 @@ public class MyZuulFilter extends ZuulFilter {
     @Override
     public Object run() {
         RequestContext context = RequestContext.getCurrentContext();
-        context.addZuulRequestHeader("n-d-version", "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0;1.2\"}");
+        // 来自于外界的Header，例如，从Postman传递过来的Header
+        String version = context.getRequest().getHeader(DiscoveryConstant.N_D_VERSION);
+        if (StringUtils.isEmpty(version)) {
+            context.addZuulRequestHeader(DiscoveryConstant.N_D_VERSION, "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0;1.2\"}");
+        }
 
         return null;
     }
