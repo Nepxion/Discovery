137873f6b7f35ebe2a7a1bea3b451ac8c00aba6d
Nepxion <1394997@qq.com>
2019-05-09 10:35:15 +0800

    简化服务和网关上下文获取Header的方式

--- discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/adapter/DefaultDiscoveryEnabledAdapter.java
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/adapter/DefaultDiscoveryEnabledAdapter.java
@@ -9,11 +9,6 @@ package com.nepxion.discovery.plugin.strategy.zuul.adapter;
  * @version 1.0
  */
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
@@ -22,65 +17,21 @@ import com.nepxion.discovery.plugin.strategy.zuul.context.ZuulStrategyContextHol
 import com.netflix.loadbalancer.Server;
 
 public class DefaultDiscoveryEnabledAdapter extends AbstractDiscoveryEnabledAdapter {
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultDiscoveryEnabledAdapter.class);
-
     @Autowired
     private ZuulStrategyContextHolder zuulStrategyContextHolder;
 
     @Override
     protected String getVersionValue(Server server) {
-        HttpServletRequest request = zuulStrategyContextHolder.getRequest();
-        if (request == null) {
-            String serviceId = pluginAdapter.getServerServiceId(server);
-
-            LOG.warn("The HttpServletRequest object is null, ignore to do version filter for service={}...", serviceId);
-
-            return null;
-        }
-
-        String version = request.getHeader(DiscoveryConstant.N_D_VERSION);
-        if (StringUtils.isEmpty(version)) {
-            version = zuulStrategyContextHolder.getZuulRequestHeaders().get(DiscoveryConstant.N_D_VERSION);
-        }
-
-        return version;
+        return zuulStrategyContextHolder.getHeader(DiscoveryConstant.N_D_VERSION);
     }
 
     @Override
     protected String getRegionValue(Server server) {
-        HttpServletRequest request = zuulStrategyContextHolder.getRequest();
-        if (request == null) {
-            String serviceId = pluginAdapter.getServerServiceId(server);
-
-            LOG.warn("The HttpServletRequest object is null, ignore to do region filter for service={}...", serviceId);
-
-            return null;
-        }
-
-        String region = request.getHeader(DiscoveryConstant.N_D_REGION);
-        if (StringUtils.isEmpty(region)) {
-            region = zuulStrategyContextHolder.getZuulRequestHeaders().get(DiscoveryConstant.N_D_REGION);
-        }
-
-        return region;
+        return zuulStrategyContextHolder.getHeader(DiscoveryConstant.N_D_REGION);
     }
 
     @Override
     protected String getAddressValue(Server server) {
-        HttpServletRequest request = zuulStrategyContextHolder.getRequest();
-        if (request == null) {
-            String serviceId = pluginAdapter.getServerServiceId(server);
-
-            LOG.warn("The HttpServletRequest object is null, ignore to do region filter for service={}...", serviceId);
-
-            return null;
-        }
-
-        String address = request.getHeader(DiscoveryConstant.N_D_ADDRESS);
-        if (StringUtils.isEmpty(address)) {
-            address = zuulStrategyContextHolder.getZuulRequestHeaders().get(DiscoveryConstant.N_D_ADDRESS);
-        }
-
-        return address;
+        return zuulStrategyContextHolder.getHeader(DiscoveryConstant.N_D_ADDRESS);
     }
 }
--- discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyContextHolder.java
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyContextHolder.java
@@ -14,9 +14,15 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.netflix.zuul.context.RequestContext;
 
 public class ZuulStrategyContextHolder {
+    private static final Logger LOG = LoggerFactory.getLogger(ZuulStrategyContextHolder.class);
+
     public HttpServletRequest getRequest() {
         HttpServletRequest request = ZuulStrategyContext.getCurrentContext().getRequest();
         if (request == null) {
@@ -28,5 +34,23 @@ public class ZuulStrategyContextHolder {
 
     public Map<String, String> getZuulRequestHeaders() {
         return RequestContext.getCurrentContext().getZuulRequestHeaders();
+    }
+
+    public String getHeader(String name) {
+        HttpServletRequest request = getRequest();
+        if (request == null) {
+            LOG.warn("The HttpServletRequest object is null");
+
+            return null;
+        }
+
+        // 来自于外界的Header，例如，从Postman传递过来的Header
+        String header = request.getHeader(name);
+        if (StringUtils.isEmpty(header)) {
+            // 来自于Zuul Filter的Header
+            header = getZuulRequestHeaders().get(name);
+        }
+
+        return header;
     }
 }
