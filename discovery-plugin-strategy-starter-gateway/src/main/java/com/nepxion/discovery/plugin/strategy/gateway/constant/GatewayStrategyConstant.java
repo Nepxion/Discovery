fe5b84487e99d8b755d8e26d3621ae5c90494183
Nepxion <1394997@qq.com>
2019-05-20 17:13:50 +0800

    增加路由策略过滤器的执行顺序配置项

---
+++ discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/constant/GatewayStrategyConstant.java
@@ -0,0 +1,14 @@
+package com.nepxion.discovery.plugin.strategy.gateway.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public class GatewayStrategyConstant {
+    public static final String SPRING_APPLICATION_STRATEGY_GATEWAY_FILTER_ORDER = "spring.application.strategy.gateway.filter.order";
+}
