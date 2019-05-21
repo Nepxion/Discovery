0eefec4a69b649fc4ab6052f2d04d354822e5e7b
Nepxion <1394997@qq.com>
2019-05-21 16:47:46 +0800

    支持在网关过滤器中从远程配置中心获取路由信息配置，并全链路传递到服务端

--- discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyZuulFilter.java
+++ discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyZuulFilter.java
@@ -9,11 +9,19 @@ package com.nepxion.discovery.plugin.example.zuul.impl;
  * @version 1.0
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.entity.RuleEntity;
+import com.nepxion.discovery.common.entity.StrategyEntity;
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
 import com.nepxion.discovery.plugin.strategy.zuul.filter.ZuulStrategyFilterResolver;
 import com.netflix.zuul.ZuulFilter;
 
 public class MyZuulFilter extends ZuulFilter {
+    @Autowired
+    private PluginAdapter pluginAdapter;
+
     @Override
     public String filterType() {
         return "pre";
@@ -31,8 +39,32 @@ public class MyZuulFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-        ZuulStrategyFilterResolver.setHeader(DiscoveryConstant.N_D_VERSION, "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0;1.2\"}");
+        String routeVersion = getRouteVersionFromConfig();
+        // String routeVersion = getRouteVersionFromCustomer();
+
+        System.out.println("Route Version=" + routeVersion);
+
+        // 通过过滤器设置路由Header头部信息，来取代界面（Postman）上的设置，并全链路传递到服务端
+        ZuulStrategyFilterResolver.setHeader(DiscoveryConstant.N_D_VERSION, routeVersion);
 
         return null;
+    }
+
+    // 从远程配置中心或者本地配置文件获取版本路由配置。如果是远程配置中心，则值会动态改变
+    protected String getRouteVersionFromConfig() {
+        RuleEntity ruleEntity = pluginAdapter.getRule();
+        if (ruleEntity != null) {
+            StrategyEntity strategyEntity = ruleEntity.getStrategyEntity();
+            if (strategyEntity != null) {
+                return strategyEntity.getVersionValue();
+            }
+        }
+
+        return null;
+    }
+
+    // 自定义版本路由配置
+    protected String getRouteVersionFromCustomer() {
+        return "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0;1.2\"}";
     }
 }
