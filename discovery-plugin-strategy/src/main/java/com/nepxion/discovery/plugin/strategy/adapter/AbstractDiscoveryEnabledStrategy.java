449ffcb83dc911d596fc9a89e11254d8c788f04d
Nepxion <1394997@qq.com>
2019-06-01 22:40:39 +0800

    修改AbstractDiscoveryEnabledStrategy抽象类，基于Header进行自定义路由更加方便

--- discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/adapter/AbstractDiscoveryEnabledStrategy.java
+++ discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/adapter/AbstractDiscoveryEnabledStrategy.java
@@ -9,31 +9,37 @@ package com.nepxion.discovery.plugin.strategy.adapter;
  * @version 1.0
  */
 
-import java.util.Map;
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
 import com.nepxion.discovery.plugin.framework.context.PluginContextAware;
 import com.nepxion.discovery.plugin.strategy.context.StrategyContextHolder;
-import com.netflix.loadbalancer.Server;
 
 public abstract class AbstractDiscoveryEnabledStrategy implements DiscoveryEnabledStrategy {
-    private StrategyContextHolder strategyContextHolder;
+    @Autowired
+    protected PluginContextAware pluginContextAware;
 
     @Autowired
-    private PluginContextAware pluginContextAware;
+    protected PluginAdapter pluginAdapter;
+
+    protected StrategyContextHolder strategyContextHolder;
 
     @PostConstruct
-    public void initialize() {
+    private void initialize() {
         strategyContextHolder = pluginContextAware.getBean(StrategyContextHolder.class);
     }
 
-    @Override
-    public boolean apply(Server server, Map<String, String> metadata) {
-        return apply(server, metadata, strategyContextHolder);
+    public PluginContextAware getPluginContextAware() {
+        return pluginContextAware;
     }
 
-    public abstract boolean apply(Server server, Map<String, String> metadata, StrategyContextHolder strategyContextHolder);
+    public PluginAdapter getPluginAdapter() {
+        return pluginAdapter;
+    }
+
+    public StrategyContextHolder getStrategyContextHolder() {
+        return strategyContextHolder;
+    }
 }
