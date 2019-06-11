e0054266c0ff123af086d02932182b4bfb304465
Nepxion <1394997@qq.com>
2019-06-11 09:25:39 +0800

    增加WebMvc和WebFlux标记

--- discovery-console/src/main/java/com/nepxion/discovery/console/context/ConsoleApplicationContextInitializer.java
+++ discovery-console/src/main/java/com/nepxion/discovery/console/context/ConsoleApplicationContextInitializer.java
@@ -1,5 +1,14 @@
 package com.nepxion.discovery.console.context;
 
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
