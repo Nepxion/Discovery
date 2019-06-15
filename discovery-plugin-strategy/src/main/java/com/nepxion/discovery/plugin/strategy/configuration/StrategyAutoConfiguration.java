3f2ebe1471dbcd0ae670afc354f82a2a3f75badd
Nepxion <1394997@qq.com>
2019-06-15 14:52:11 +0800

    路由Header增加通配符功能

--- discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/configuration/StrategyAutoConfiguration.java
+++ discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/configuration/StrategyAutoConfiguration.java
@@ -9,15 +9,23 @@ package com.nepxion.discovery.plugin.strategy.configuration;
  * @version 1.0
  */
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.nepxion.discovery.plugin.strategy.constant.StrategyConstant;
+import com.nepxion.discovery.plugin.strategy.matcher.DiscoveryAntPathMatcherStrategy;
+import com.nepxion.discovery.plugin.strategy.matcher.DiscoveryMatcherStrategy;
 
 @Configuration
 @RibbonClients(defaultConfiguration = { StrategyLoadBalanceConfiguration.class })
 @ConditionalOnProperty(value = StrategyConstant.SPRING_APPLICATION_STRATEGY_CONTROL_ENABLED, matchIfMissing = true)
 public class StrategyAutoConfiguration {
-
+    @Bean
+    @ConditionalOnMissingBean
+    public DiscoveryMatcherStrategy discoveryMatcherStrategy() {
+        return new DiscoveryAntPathMatcherStrategy();
+    }
 }
