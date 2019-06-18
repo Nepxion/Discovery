9a7822e68c9720935b44b1dd1e314f43319a74bf
Nepxion <1394997@qq.com>
2019-06-18 09:18:46 +0800

    优化类结构

--- discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyRouteFilter.java
+++ discovery-springcloud-example-zuul/src/main/java/com/nepxion/discovery/plugin/example/zuul/impl/MyRouteFilter.java
@@ -10,19 +10,30 @@ package com.nepxion.discovery.plugin.example.zuul.impl;
  */
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.nepxion.discovery.plugin.strategy.zuul.filter.ZuulStrategyRouteFilter;
 
-// 当Header中传来的用户为张三，执行一条路由路径；李四，执行另一条路由路径
+// 适用于A/B Testing或者更根据某业务参数决定灰度路由路径。可以结合配置中心分别配置A/B两条路径，可以动态改变并通知
+// 当Header中传来的用户为张三，执行一条路由路径；为李四，执行另一条路由路径
 public class MyRouteFilter extends ZuulStrategyRouteFilter {
+    private static final String DEFAULT_A_ROUTE_VERSION = "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0\"}";
+    private static final String DEFAULT_B_ROUTE_VERSION = "{\"discovery-springcloud-example-a\":\"1.1\", \"discovery-springcloud-example-b\":\"1.1\", \"discovery-springcloud-example-c\":\"1.1\"}";
+
+    @Value("${a.route.version:" + DEFAULT_A_ROUTE_VERSION + "}")
+    private String aRouteVersion;
+
+    @Value("${b.route.version:" + DEFAULT_B_ROUTE_VERSION + "}")
+    private String bRouteVersion;
+
     @Override
     protected String getRouteVersion() {
         String user = strategyContextHolder.getHeader("user");
 
         if (StringUtils.equals(user, "zhangsan")) {
-            return "{\"discovery-springcloud-example-a\":\"1.0\", \"discovery-springcloud-example-b\":\"1.0\", \"discovery-springcloud-example-c\":\"1.0\"}";
+            return aRouteVersion;
         } else if (StringUtils.equals(user, "lisi")) {
-            return "{\"discovery-springcloud-example-a\":\"1.1\", \"discovery-springcloud-example-b\":\"1.1\", \"discovery-springcloud-example-c\":\"1.1\"}";
+            return bRouteVersion;
         }
 
         return super.getRouteVersion();
