8370bc939816291ce7d84cb5d13b6179ea6e05c8
Nepxion <1394997@qq.com>
2019-06-19 18:34:32 +0800

    增加提供端的服务隔离

---
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/isolation/ProviderIsolationStrategyInterceptor.java
@@ -0,0 +1,39 @@
+package com.nepxion.discovery.plugin.strategy.service.isolation;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.exception.DiscoveryException;
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
+import com.nepxion.discovery.plugin.strategy.service.context.ServiceStrategyContextHolder;
+import com.nepxion.matrix.proxy.aop.AbstractInterceptor;
+
+public class ProviderIsolationStrategyInterceptor extends AbstractInterceptor {
+    @Autowired
+    private ServiceStrategyContextHolder serviceStrategyContextHolder;
+
+    @Autowired
+    private PluginAdapter pluginAdapter;
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        String groupHeader = serviceStrategyContextHolder.getHeader(DiscoveryConstant.N_D_GROUP);
+        String group = pluginAdapter.getGroup();
+        if (!StringUtils.equals(groupHeader, group)) {
+            throw new DiscoveryException("Not allow to invoke for isolation with different service group");
+        }
+
+        return invocation.proceed();
+    }
+}
