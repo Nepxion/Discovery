d12fd37676cc3c191f897e015104f10dc297aa7d
Nepxion <1394997@qq.com>
2019-06-24 17:01:00 +0800

    增加随机权重路由

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
@@ -19,20 +19,20 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-import com.nepxion.discovery.common.entity.WeightFilterEntity;
 import com.nepxion.discovery.common.exception.DiscoveryException;
+import com.nepxion.discovery.plugin.framework.loadbalance.WeightRandomLoadBalance;
 import com.netflix.loadbalancer.Server;
 
-public class MapWeightRandomLoadBalance extends AbstractWeightRandomLoadBalance {
+public abstract class AbstractMapWeightRandomLoadBalance<T> implements WeightRandomLoadBalance<T> {
     @Override
-    public Server choose(List<Server> serverList, WeightFilterEntity weightFilterEntity) {
+    public Server choose(List<Server> serverList, T t) {
         if (CollectionUtils.isEmpty(serverList)) {
             return null;
         }
 
         List<Pair<Server, Integer>> weightPairList = new ArrayList<Pair<Server, Integer>>();
         for (Server server : serverList) {
-            int weight = getWeight(server, weightFilterEntity);
+            int weight = getWeight(server, t);
             weightPairList.add(new ImmutablePair<Server, Integer>(server, weight));
         }
 
