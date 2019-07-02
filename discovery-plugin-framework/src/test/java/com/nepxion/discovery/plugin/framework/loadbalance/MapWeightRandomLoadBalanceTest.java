19c82670e2e140cf209da0d2fb8fa6a951101696
Nepxion <1394997@qq.com>
2019-07-02 14:33:45 +0800

    修改测试用例

--- discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
+++ discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
@@ -1,5 +1,14 @@
 package com.nepxion.discovery.plugin.framework.loadbalance;
 
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedMap;
