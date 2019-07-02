a28132dbb63bd06081ae7af7f4b28e5d82f34dad
Nepxion <1394997@qq.com>
2019-07-02 09:17:53 +0800

    优化Header传递机制

--- discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/filter/AbstractGatewayStrategyRouteFilter.java
+++ discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/filter/AbstractGatewayStrategyRouteFilter.java
@@ -56,53 +56,29 @@ public abstract class AbstractGatewayStrategyRouteFilter implements GlobalFilter
         // 通过过滤器设置路由Header头部信息，并全链路传递到服务端
         ServerHttpRequest.Builder requestBuilder = exchange.getRequest().mutate();
         if (StringUtils.isNotEmpty(routeVersion)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_VERSION));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_VERSION, routeVersion);
+            GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_VERSION, routeVersion, gatewayHeaderPriority);
         }
         if (StringUtils.isNotEmpty(routeRegion)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_REGION));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_REGION, routeRegion);
+            GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_REGION, routeRegion, gatewayHeaderPriority);
         }
         if (StringUtils.isNotEmpty(routeAddress)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_ADDRESS));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_ADDRESS, routeAddress);
+            GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_ADDRESS, routeAddress, gatewayHeaderPriority);
         }
         if (StringUtils.isNotEmpty(routeVersionWeight)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_VERSION_WEIGHT));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_VERSION_WEIGHT, routeVersionWeight);
+            GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_VERSION_WEIGHT, routeVersionWeight, gatewayHeaderPriority);
         }
         if (StringUtils.isNotEmpty(routeRegionWeight)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_REGION_WEIGHT));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_REGION_WEIGHT, routeRegionWeight);
+            GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_REGION_WEIGHT, routeRegionWeight, gatewayHeaderPriority);
         }
 
-        // 开启此项，将启动提供端的服务隔离机制，则传递到服务的是网关自身ServiceType，ServiceId，Group
-        if (environment.getProperty(StrategyConstant.SPRING_APPLICATION_STRATEGY_PROVIDER_ISOLATION_ENABLED, Boolean.class, Boolean.FALSE)) {
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_SERVICE_TYPE));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_SERVICE_TYPE, pluginAdapter.getServiceType());
+        // 开启此项，将启动提供端的服务隔离机制，则传递到服务的是网关自身ServiceType, ServiceId, ServiceHost, Group
+        // 如果不隔离，为了调用链完整，最好外界自身传递ServiceType, ServiceId, ServiceHost, Group
+        Boolean providerIsolationEnabled = environment.getProperty(StrategyConstant.SPRING_APPLICATION_STRATEGY_PROVIDER_ISOLATION_ENABLED, Boolean.class, Boolean.FALSE);
 
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_SERVICE_ID));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_SERVICE_ID, pluginAdapter.getServiceId());
-
-            if (gatewayHeaderPriority) {
-                requestBuilder.headers(headers -> headers.remove(DiscoveryConstant.N_D_GROUP));
-            }
-            requestBuilder.header(DiscoveryConstant.N_D_GROUP, pluginAdapter.getGroup());
-        }
+        GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_SERVICE_TYPE, pluginAdapter.getServiceType(), providerIsolationEnabled ? providerIsolationEnabled : gatewayHeaderPriority);
+        GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_SERVICE_ID, pluginAdapter.getServiceId(), providerIsolationEnabled ? providerIsolationEnabled : gatewayHeaderPriority);
+        GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_SERVICE_HOST, pluginAdapter.getHost() + ":" + pluginAdapter.getPort(), providerIsolationEnabled ? providerIsolationEnabled : gatewayHeaderPriority);
+        GatewayStrategyFilterResolver.setHeader(requestBuilder, DiscoveryConstant.N_D_GROUP, pluginAdapter.getGroup(), providerIsolationEnabled ? providerIsolationEnabled : gatewayHeaderPriority);
 
         ServerHttpRequest newRequest = requestBuilder.build();
         ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
