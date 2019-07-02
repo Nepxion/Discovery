3f5f7170f7a45167e160b507fd29fe116bb509cc
Nepxion <1394997@qq.com>
2019-07-02 16:22:10 +0800

    修改测试用例

--- discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
+++ discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
@@ -9,6 +9,7 @@ package com.nepxion.discovery.plugin.framework.loadbalance;
  * @version 1.0
  */
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedMap;
@@ -21,6 +22,62 @@ import org.apache.commons.lang3.tuple.Pair;
 import com.nepxion.discovery.common.exception.DiscoveryException;
 
 public class MapWeightRandomLoadBalanceTest {
+    public static void main(String[] args) {
+        test(1000000);
+    }
+
+    public static void test(int totolCount) {
+        long t = System.currentTimeMillis();
+
+        List<Pair<String, Double>> list = new ArrayList<Pair<String, Double>>();
+        list.add(new ImmutablePair<String, Double>("1.0", 10D));
+        list.add(new ImmutablePair<String, Double>("2.0", 50D));
+        list.add(new ImmutablePair<String, Double>("3.0", 20D));
+        list.add(new ImmutablePair<String, Double>("1.0", 10D));
+        list.add(new ImmutablePair<String, Double>("2.0", 50D));
+        list.add(new ImmutablePair<String, Double>("3.0", 20D));
+        list.add(new ImmutablePair<String, Double>("4.0", 5D));
+        list.add(new ImmutablePair<String, Double>("4.0", 5D));
+        list.add(new ImmutablePair<String, Double>("5.0", 15D));
+        list.add(new ImmutablePair<String, Double>("5.0", 15D));
+
+        int v1Count = 0;
+        int v2Count = 0;
+        int v3Count = 0;
+        int v4Count = 0;
+        int v5Count = 0;
+        for (int i = 0; i < totolCount; i++) {
+            MapWeightRandom<String, Double> weightRandom = new MapWeightRandom<String, Double>(list);
+            String server = weightRandom.random();
+            if (server.startsWith("1.0")) {
+                v1Count++;
+            }
+            if (server.startsWith("2.0")) {
+                v2Count++;
+            }
+            if (server.startsWith("3.0")) {
+                v3Count++;
+            }
+            if (server.startsWith("4.0")) {
+                v4Count++;
+            }
+            if (server.startsWith("5.0")) {
+                v5Count++;
+            }
+        }
+
+        System.out.println("------------------------------");
+        System.out.println(totolCount + "次循环，散列方式随机权重准确度和性能：");
+        DecimalFormat format = new DecimalFormat("0.0000");
+        System.out.println("1.0版本服务随机权重=" + format.format((double) v1Count * 100 / totolCount) + "%");
+        System.out.println("2.0版本服务随机权重=" + format.format((double) v2Count * 100 / totolCount) + "%");
+        System.out.println("3.0版本服务随机权重=" + format.format((double) v3Count * 100 / totolCount) + "%");
+        System.out.println("4.0版本服务随机权重=" + format.format((double) v4Count * 100 / totolCount) + "%");
+        System.out.println("5.0版本服务随机权重=" + format.format((double) v5Count * 100 / totolCount) + "%");
+        System.out.println("耗时时间：" + (System.currentTimeMillis() - t));
+        System.out.println("------------------------------");
+    }
+
     public static class MapWeightRandom<K, V extends Number> {
         private TreeMap<Double, K> weightMap = new TreeMap<Double, K>();
 
@@ -46,53 +103,5 @@ public class MapWeightRandomLoadBalanceTest {
 
             return weightMap.get(tailMap.firstKey());
         }
-    }
-
-    public static void main(String[] args) {
-        long t = System.currentTimeMillis();
-
-        List<Pair<String, Double>> list = new ArrayList<Pair<String, Double>>();
-        list.add(new ImmutablePair<String, Double>("1.0", 0D));
-        list.add(new ImmutablePair<String, Double>("2.0", 90D));
-        list.add(new ImmutablePair<String, Double>("3.0", 10D));
-        list.add(new ImmutablePair<String, Double>("1.0", 0D));
-        list.add(new ImmutablePair<String, Double>("2.0", 90D));
-        list.add(new ImmutablePair<String, Double>("3.0", 10D));
-        list.add(new ImmutablePair<String, Double>("4.0", 0D));
-        list.add(new ImmutablePair<String, Double>("4.0", 0D));
-        list.add(new ImmutablePair<String, Double>("5.0", 0D));
-        list.add(new ImmutablePair<String, Double>("5.0", 0D));
-
-        int v1Count = 0;
-        int v2Count = 0;
-        int v3Count = 0;
-        int v4Count = 0;
-        int v5Count = 0;
-        for (int i = 0; i < 1000000; i++) {
-            MapWeightRandom<String, Double> weightRandom = new MapWeightRandom<String, Double>(list);
-            String server = weightRandom.random();
-            if (server.startsWith("1.0")) {
-                v1Count++;
-            }
-            if (server.startsWith("2.0")) {
-                v2Count++;
-            }
-            if (server.startsWith("3.0")) {
-                v3Count++;
-            }
-            if (server.startsWith("4.0")) {
-                v4Count++;
-            }
-            if (server.startsWith("5.0")) {
-                v5Count++;
-            }
-        }
-
-        System.out.println("1.0版本服务随机权重=" + v1Count);
-        System.out.println("2.0版本服务随机权重=" + v2Count);
-        System.out.println("3.0版本服务随机权重=" + v3Count);
-        System.out.println("4.0版本服务随机权重=" + v4Count);
-        System.out.println("5.0版本服务随机权重=" + v5Count);
-        System.out.println("耗时时间：" + (System.currentTimeMillis() - t));
     }
 }
