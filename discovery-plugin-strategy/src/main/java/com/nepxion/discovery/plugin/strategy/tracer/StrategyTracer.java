867e7401e05a378067d415c81dbb7945b7e0a262
Nepxion <1394997@qq.com>
2019-07-04 18:01:31 +0800

    增强调用链增强调用链

---
+++ discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/tracer/StrategyTracer.java
@@ -0,0 +1,69 @@
+package com.nepxion.discovery.plugin.strategy.tracer;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
+import com.nepxion.discovery.plugin.strategy.constant.StrategyConstant;
+import com.nepxion.discovery.plugin.strategy.context.StrategyContextHolder;
+
+public class StrategyTracer {
+    @Autowired
+    private ConfigurableEnvironment environment;
+
+    @Autowired
+    protected PluginAdapter pluginAdapter;
+
+    @Autowired
+    protected StrategyContextHolder strategyContextHolder;
+
+    public void traceHeader() {
+        Boolean traceLogPrint = environment.getProperty(StrategyConstant.SPRING_APPLICATION_STRATEGY_TRACE_LOG_PRINT, Boolean.class, Boolean.FALSE);
+        if (!traceLogPrint) {
+            return;
+        }
+
+        System.out.println("---------------- Trace Information ---------------");
+        System.out.println(DiscoveryConstant.N_D_SERVICE_TYPE + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_TYPE));
+        System.out.println(DiscoveryConstant.N_D_SERVICE_ID + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ID));
+        System.out.println(DiscoveryConstant.N_D_SERVICE_ADDRESS + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_ADDRESS));
+        System.out.println(DiscoveryConstant.N_D_SERVICE_GROUP + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_GROUP));
+        System.out.println(DiscoveryConstant.N_D_SERVICE_VERSION + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_VERSION));
+        System.out.println(DiscoveryConstant.N_D_SERVICE_REGION + "=" + strategyContextHolder.getHeader(DiscoveryConstant.N_D_SERVICE_REGION));
+        System.out.println("--------------------------------------------------");
+    }
+
+    public void traceInvoker() {
+        Boolean interceptLogPrint = environment.getProperty(StrategyConstant.SPRING_APPLICATION_STRATEGY_TRACE_LOG_PRINT, Boolean.class, Boolean.FALSE);
+        if (!interceptLogPrint) {
+            return;
+        }
+
+        System.out.println("---------------- Trace Information ---------------");
+        System.out.println(DiscoveryConstant.N_D_SERVICE_TYPE + "=" + pluginAdapter.getServiceType());
+        System.out.println(DiscoveryConstant.N_D_SERVICE_ID + "=" + pluginAdapter.getServiceId());
+        System.out.println(DiscoveryConstant.N_D_SERVICE_ADDRESS + "=" + pluginAdapter.getHost() + ":" + pluginAdapter.getPort());
+        System.out.println(DiscoveryConstant.N_D_SERVICE_GROUP + "=" + pluginAdapter.getGroup());
+        System.out.println(DiscoveryConstant.N_D_SERVICE_VERSION + "=" + pluginAdapter.getVersion());
+        System.out.println(DiscoveryConstant.N_D_SERVICE_REGION + "=" + pluginAdapter.getRegion());
+        System.out.println("--------------------------------------------------");
+    }
+
+    public PluginAdapter getPluginAdapter() {
+        return pluginAdapter;
+    }
+
+    public StrategyContextHolder getStrategyContextHolder() {
+        return strategyContextHolder;
+    }
+}
