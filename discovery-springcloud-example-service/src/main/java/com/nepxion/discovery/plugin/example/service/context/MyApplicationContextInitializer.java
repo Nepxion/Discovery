cc3d0050d092eace618585eecb8fe6f149eb3a28
Nepxion <1394997@qq.com>
2019-08-11 11:13:52 +0800

    优化示例

--- discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/context/MyApplicationContextInitializer.java
+++ discovery-springcloud-example-service/src/main/java/com/nepxion/discovery/plugin/example/service/context/MyApplicationContextInitializer.java
@@ -9,18 +9,20 @@ package com.nepxion.discovery.plugin.example.service.context;
  * @version 1.0
  */
 
-import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 通过如下方式，可以代替在配置文件里面配置元数据，格式为ext.xxx。例如，如果version希望通过git插件获取到最后一次提交编号，可以动态在这里进行解析并放入
 public class MyApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        if (applicationContext instanceof AnnotationConfigEmbeddedWebApplicationContext) {
-            // System.setProperty("ext.group", "myGroup");
-            // System.setProperty("ext.version", "8888");
-            // System.setProperty("ext.region", "myRegion");
+        if (applicationContext instanceof AnnotationConfigApplicationContext) {
+            return;
         }
+
+        // System.setProperty("ext.group", "myGroup");
+        // System.setProperty("ext.version", "8888");
+        // System.setProperty("ext.region", "myRegion");
     }
 }
