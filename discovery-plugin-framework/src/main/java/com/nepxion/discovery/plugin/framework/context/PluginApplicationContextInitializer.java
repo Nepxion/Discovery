e52b2d2df24adf6ef107f751f6a0d20f09d81b0d
Nepxion <1394997@qq.com>
2019-08-11 10:46:43 +0800

    增加内置配置文件的功能

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginApplicationContextInitializer.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginApplicationContextInitializer.java
@@ -9,11 +9,19 @@ package com.nepxion.discovery.plugin.framework.context;
  * @version 1.0
  */
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.nepxion.banner.BannerConstant;
 import com.nepxion.banner.Description;
@@ -21,9 +29,12 @@ import com.nepxion.banner.LogoBanner;
 import com.nepxion.banner.NepxionBanner;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.plugin.framework.decorator.DiscoveryClientDecorator;
+import com.nepxion.discovery.plugin.framework.property.PluginProperties;
 import com.taobao.text.Color;
 
 public abstract class PluginApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static final Logger LOG = LoggerFactory.getLogger(PluginApplicationContextInitializer.class);
+
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
         String applicationContextClassName = applicationContext.getClass().getName();
@@ -67,6 +78,33 @@ public abstract class PluginApplicationContextInitializer implements Application
                 }
             }
         });
+
+        initializeDefaultProperties(applicationContext);
+    }
+
+    private void initializeDefaultProperties(ConfigurableApplicationContext applicationContext) {
+        if (applicationContext instanceof AnnotationConfigApplicationContext) {
+            return;
+        }
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String path = PluginContextAware.getDefaultPropertiesPath(environment);
+
+        try {
+            PluginProperties properties = new PluginProperties(path, DiscoveryConstant.ENCODING_GBK, DiscoveryConstant.ENCODING_UTF_8);
+            Map<String, String> propertiesMap = properties.getMap();
+            for (Map.Entry<String, String> entry : propertiesMap.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                String property = environment.getProperty(key);
+                if (StringUtils.isEmpty(property)) {
+                    System.setProperty(key, value);
+                }
+            }
+            LOG.info("{} is loaded", path);
+        } catch (IOException e) {
+
+        }
     }
 
     protected abstract Object afterInitialization(ConfigurableApplicationContext applicationContext, Object bean, String beanName) throws BeansException;
