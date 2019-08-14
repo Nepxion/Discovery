09eafd9af68d5f8346eae01818aa88471e4ce82f
Nepxion <1394997@qq.com>
2019-08-14 09:24:43 +0800

    增加自动化测试模块

---
+++ discovery-plugin-test-automation/src/main/java/com/nepxion/discovery/plugin/test/automation/constant/TestConstant.java
@@ -0,0 +1,18 @@
+package com.nepxion.discovery.plugin.test.automation.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public class TestConstant {
+    public static final String SPRING_APPLICATION_TEST_SCAN_PACKAGES = "spring.application.test.scan.packages";
+    public static final String SPRING_APPLICATION_TEST_GRAY_CONFIGCENTER_ENABLED = "spring.application.test.gray.configcenter.enabled";
+    public static final String SPRING_APPLICATION_TEST_GRAY_RESET_ENABLED = "spring.application.test.gray.reset.enabled";
+    public static final String SPRING_APPLICATION_TEST_GRAY_AWAIT_TIME = "spring.application.test.gray.await.time";
+    public static final String SPRING_APPLICATION_TEST_CONSOLE_URL = "spring.application.test.console.url";
+}
