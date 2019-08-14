63ae3e7177145766e08dd9d8c27022ef7774ea82
Nepxion <1394997@qq.com>
2019-08-14 22:03:55 +0800

    修改拼写错误

--- discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
+++ discovery-plugin-framework/src/test/java/com/nepxion/discovery/plugin/framework/loadbalance/MapWeightRandomLoadBalanceTest.java
@@ -26,7 +26,7 @@ public class MapWeightRandomLoadBalanceTest {
         test(1000000);
     }
 
-    public static void test(int totolCount) {
+    public static void test(int totalCount) {
         long t = System.currentTimeMillis();
 
         List<Pair<String, Double>> list = new ArrayList<Pair<String, Double>>();
@@ -46,7 +46,7 @@ public class MapWeightRandomLoadBalanceTest {
         int v3Count = 0;
         int v4Count = 0;
         int v5Count = 0;
-        for (int i = 0; i < totolCount; i++) {
+        for (int i = 0; i < totalCount; i++) {
             MapWeightRandom<String, Double> weightRandom = new MapWeightRandom<String, Double>(list);
             String server = weightRandom.random();
             if (server.startsWith("1.0")) {
@@ -67,13 +67,13 @@ public class MapWeightRandomLoadBalanceTest {
         }
 
         System.out.println("------------------------------");
-        System.out.println(totolCount + "次循环，散列方式随机权重准确度和性能：");
+        System.out.println(totalCount + "次循环，散列方式随机权重准确度和性能：");
         DecimalFormat format = new DecimalFormat("0.0000");
-        System.out.println("1.0版本服务随机权重=" + format.format((double) v1Count * 100 / totolCount) + "%");
-        System.out.println("2.0版本服务随机权重=" + format.format((double) v2Count * 100 / totolCount) + "%");
-        System.out.println("3.0版本服务随机权重=" + format.format((double) v3Count * 100 / totolCount) + "%");
-        System.out.println("4.0版本服务随机权重=" + format.format((double) v4Count * 100 / totolCount) + "%");
-        System.out.println("5.0版本服务随机权重=" + format.format((double) v5Count * 100 / totolCount) + "%");
+        System.out.println("1.0版本服务随机权重=" + format.format((double) v1Count * 100 / totalCount) + "%");
+        System.out.println("2.0版本服务随机权重=" + format.format((double) v2Count * 100 / totalCount) + "%");
+        System.out.println("3.0版本服务随机权重=" + format.format((double) v3Count * 100 / totalCount) + "%");
+        System.out.println("4.0版本服务随机权重=" + format.format((double) v4Count * 100 / totalCount) + "%");
+        System.out.println("5.0版本服务随机权重=" + format.format((double) v5Count * 100 / totalCount) + "%");
         System.out.println("耗时时间：" + (System.currentTimeMillis() - t));
         System.out.println("------------------------------");
     }
