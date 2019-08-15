fbe42bb9c517cc36ead7a10f2fdd5afff5c7ba7f
Nepxion <1394997@qq.com>
2019-08-15 20:53:03 +0800

    升级版本到3.11.6

--- discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
+++ discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
@@ -10,7 +10,7 @@ package com.nepxion.discovery.common.constant;
  */
 
 public class DiscoveryConstant {
-    public static final String DISCOVERY_VERSION = "3.11.5";
+    public static final String DISCOVERY_VERSION = "3.11.6";
 
     public static final String SPRING_APPLICATION_DISCOVERY_PLUGIN = "spring.application.discovery.plugin";
     public static final String SPRING_APPLICATION_DISCOVERY_VERSION = "spring.application.discovery.version";
--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DnewVersion=3.11.5
+call mvn versions:set -DnewVersion=3.11.6
 
 pause
