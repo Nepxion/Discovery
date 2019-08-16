88782931160e5b15813c9b34b7270f7a7af67b26
Nepxion <1394997@qq.com>
2019-08-16 19:29:34 +0800

    修复路由接口的Bug

--- discovery-plugin-admin-center/src/main/java/com/nepxion/discovery/plugin/admincenter/endpoint/RouterEndpoint.java
+++ discovery-plugin-admin-center/src/main/java/com/nepxion/discovery/plugin/admincenter/endpoint/RouterEndpoint.java
@@ -124,7 +124,7 @@ public class RouterEndpoint {
         String region = pluginAdapter.getRegion();
         String host = pluginAdapter.getHost();
         int port = pluginAdapter.getPort();
-        int weight = getWeight(serviceId, version, region);
+        int weight = -1;
         String contextPath = pluginAdapter.getContextPath();
 
         RouterEntity routerEntity = new RouterEntity();
