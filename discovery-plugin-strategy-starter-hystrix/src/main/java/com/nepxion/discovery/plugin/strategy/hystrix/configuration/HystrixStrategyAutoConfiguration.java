87ef89ae1f914f6955e69fb944197dd596078704
Nepxion <1394997@qq.com>
2019-09-14 23:02:50 +0800

    增加旗标

--- discovery-plugin-strategy-starter-hystrix/src/main/java/com/nepxion/discovery/plugin/strategy/hystrix/configuration/HystrixStrategyAutoConfiguration.java
+++ discovery-plugin-strategy-starter-hystrix/src/main/java/com/nepxion/discovery/plugin/strategy/hystrix/configuration/HystrixStrategyAutoConfiguration.java
@@ -15,14 +15,25 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
 import com.nepxion.discovery.plugin.strategy.constant.StrategyConstant;
 import com.nepxion.discovery.plugin.strategy.hystrix.context.HystrixContextConcurrencyStrategy;
 import com.netflix.hystrix.Hystrix;
+import com.taobao.text.Color;
 
 @Configuration
 @ConditionalOnClass(Hystrix.class)
 @ConditionalOnProperty(value = StrategyConstant.SPRING_APPLICATION_STRATEGY_HYSTRIX_THREADLOCAL_SUPPORTED, matchIfMissing = false)
 public class HystrixStrategyAutoConfiguration {
+    static {
+        LogoBanner logoBanner = new LogoBanner(HystrixStrategyAutoConfiguration.class, "/com/nepxion/hystrix/resource/logo.txt", "Welcome to Nepxion", 7, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red }, true);
+
+        NepxionBanner.show(logoBanner, new Description("Protect:", "Hystrix", 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Discovery", 0, 1));
+    }
+
     @Bean
     public HystrixContextConcurrencyStrategy hystrixContextConcurrencyStrategy() {
         return new HystrixContextConcurrencyStrategy();
