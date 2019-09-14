8fd2518f75865cec8a19f36d4109b79d540da4cc
wangwh <wangwh@fenqi.im>
2019-09-14 12:35:43 +0800

    对接apollo配置中心

--- discovery-plugin-strategy-sentinel-starter-apollo/src/main/java/com/nepxion/discovery/sentinel/starter/apollo/configuration/SentinelApolloAutoConfiguration.java
+++ discovery-plugin-strategy-sentinel-starter-apollo/src/main/java/com/nepxion/discovery/sentinel/starter/apollo/configuration/SentinelApolloAutoConfiguration.java
@@ -2,7 +2,9 @@ package com.nepxion.discovery.sentinel.starter.apollo.configuration;
 
 import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
 import com.nepxion.discovery.plugin.strategy.constant.StrategyConstant;
+import com.nepxion.discovery.plugin.strategy.sentinel.constant.SentinelConstant;
 import com.nepxion.discovery.plugin.strategy.sentinel.loader.RuleLoader;
+import com.nepxion.discovery.sentinel.starter.apollo.constant.ApolloSentinelConstant;
 import com.nepxion.discovery.sentinel.starter.apollo.loader.ApolloRuleLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
 public class SentinelApolloAutoConfiguration {
 
   /** Apollo 特有的命名空间 */
-  @Value("")
+  @Value("${" + ApolloSentinelConstant.APOLLO_NAMESPACE_KEY + ":" + ApolloSentinelConstant.DEFAULT_APOLLO_NAMESPACE + "}")
   private String namespace;
 
   @Autowired
@@ -28,20 +30,21 @@ public class SentinelApolloAutoConfiguration {
   /* 以下参数已在SentinelStrategyAutoConfiguration类中初始化，再次初始化是因为以下文件中的json将作为默认规则 */
 
   /** 流控规则文件路径 */
-  @Value("")
+  @Value("${" + SentinelConstant.SENTINEL_FLOW_PATH + ":file:" + SentinelConstant.SENTINEL_FLOW_KEY + ".json}")
   private String flowPath;
   /** 降级规则文件路径 */
-  @Value("")
+  @Value("${" + SentinelConstant.SENTINEL_SYSTEM_PATH + ":file:" + SentinelConstant.SENTINEL_SYSTEM_KEY + ".json}")
   private String degradePath;
   /** 授权规则文件路径 */
-  @Value("")
+  @Value("${" + SentinelConstant.SENTINEL_AUTH_PATH + ":file:" + SentinelConstant.SENTINEL_AUTH_KEY + ".json}")
   private String authorityPath;
   /** 系统规则文件路径 */
-  @Value("")
+  @Value("${" + SentinelConstant.SENTINEL_DEGRADE_PATH + ":file:" + SentinelConstant.SENTINEL_DEGRADE_KEY + ".json}")
   private String systemPath;
   /** 热点参数流控规则文件路径 */
-  @Value("")
+  @Value("${" + SentinelConstant.SENTINEL_PARAM_PATH + ":file:" + SentinelConstant.SENTINEL_PARAM_KEY + ".json}")
   private String paramPath;
+
 
   @Bean
   public RuleLoader ruleLoader(){
---
+++ discovery-plugin-strategy-sentinel-starter-apollo/src/main/java/com/nepxion/discovery/sentinel/starter/apollo/constant/ApolloSentinelConstant.java
@@ -0,0 +1,11 @@
+package com.nepxion.discovery.sentinel.starter.apollo.constant;
+
+/**
+ * @author Weihua
+ * @since 1.0.0
+ */
+public class ApolloSentinelConstant {
+
+  public static final String APOLLO_NAMESPACE_KEY = "spring.application.strategy.sentinel.apollo.namespace";
+  public static final String DEFAULT_APOLLO_NAMESPACE = "application";
+}
