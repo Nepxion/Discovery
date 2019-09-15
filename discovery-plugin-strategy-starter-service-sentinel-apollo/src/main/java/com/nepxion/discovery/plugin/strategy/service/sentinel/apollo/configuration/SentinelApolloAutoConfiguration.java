12ef22ef86c129004d2e065e6e8659ac729f5960
Nepxion <1394997@qq.com>
2019-09-15 19:36:52 +0800

    抽象类结构

--- discovery-plugin-strategy-starter-service-sentinel-apollo/src/main/java/com/nepxion/discovery/plugin/strategy/service/sentinel/apollo/configuration/SentinelApolloAutoConfiguration.java
+++ discovery-plugin-strategy-starter-service-sentinel-apollo/src/main/java/com/nepxion/discovery/plugin/strategy/service/sentinel/apollo/configuration/SentinelApolloAutoConfiguration.java
@@ -14,13 +14,25 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.apollo.loader.SentinelApolloRuleLoader;
+import com.nepxion.discovery.plugin.strategy.service.sentinel.configuration.SentinelStrategyAutoConfiguration;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.constant.SentinelStrategyConstant;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.loader.SentinelRuleLoader;
+import com.taobao.text.Color;
 
 @Configuration
 @ConditionalOnProperty(value = SentinelStrategyConstant.SPRING_APPLICATION_STRATEGY_SENTINEL_ENABLED, matchIfMissing = false)
 public class SentinelApolloAutoConfiguration {
+    static {
+        LogoBanner logoBanner = new LogoBanner(SentinelStrategyAutoConfiguration.class, "/com/nepxion/sentinel/resource/logo.txt", "Welcome to Nepxion", 8, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red, Color.green }, true);
+
+        NepxionBanner.show(logoBanner, new Description("Protect:", "Sentinel", 0, 1), new Description("Config:", "Apollo", 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Discovery", 0, 1));
+    }
+
     @Bean
     public SentinelRuleLoader sentinelRuleLoader() {
         return new SentinelApolloRuleLoader();
