06d266affe17d275b212cbda4007eb9cb5649e0f
Nepxion <1394997@qq.com>
2019-09-15 19:34:45 +0800

    抽象类结构

--- discovery-plugin-strategy-service-sentinel/src/main/java/com/nepxion/discovery/plugin/strategy/service/sentinel/configuration/SentinelStrategyAutoConfiguration.java
+++ discovery-plugin-strategy-service-sentinel/src/main/java/com/nepxion/discovery/plugin/strategy/service/sentinel/configuration/SentinelStrategyAutoConfiguration.java
@@ -20,24 +20,13 @@ import org.springframework.context.annotation.Configuration;
 import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
 import com.alibaba.csp.sentinel.adapter.servlet.callback.RequestOriginParser;
 import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
-import com.nepxion.banner.BannerConstant;
-import com.nepxion.banner.Description;
-import com.nepxion.banner.LogoBanner;
-import com.nepxion.banner.NepxionBanner;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.constant.SentinelStrategyConstant;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.loader.SentinelRuleLoader;
 import com.nepxion.discovery.plugin.strategy.service.sentinel.parser.SentinelRequestOriginParser;
-import com.taobao.text.Color;
 
 @Configuration
 @ConditionalOnProperty(value = SentinelStrategyConstant.SPRING_APPLICATION_STRATEGY_SENTINEL_ENABLED, matchIfMissing = false)
 public class SentinelStrategyAutoConfiguration {
-    static {
-        LogoBanner logoBanner = new LogoBanner(SentinelStrategyAutoConfiguration.class, "/com/nepxion/sentinel/resource/logo.txt", "Welcome to Nepxion", 8, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red, Color.green }, true);
-
-        NepxionBanner.show(logoBanner, new Description("Protect:", "Sentinel", 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Discovery", 0, 1));
-    }
-
     @Autowired
     private SentinelRuleLoader sentinelRuleLoader;
 
