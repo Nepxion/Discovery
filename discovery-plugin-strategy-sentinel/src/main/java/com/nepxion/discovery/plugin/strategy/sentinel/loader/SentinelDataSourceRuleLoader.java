d4d41ac09b8e8b6a922038eb7afd72eb6f97b6bb
Nepxion <1394997@qq.com>
2019-09-17 16:46:13 +0800

    重构Sentinel模块

--- discovery-plugin-strategy-sentinel/src/main/java/com/nepxion/discovery/plugin/strategy/sentinel/loader/SentinelDataSourceRuleLoader.java
+++ discovery-plugin-strategy-sentinel/src/main/java/com/nepxion/discovery/plugin/strategy/sentinel/loader/SentinelDataSourceRuleLoader.java
@@ -77,5 +77,7 @@ public abstract class SentinelDataSourceRuleLoader extends SentinelFileRuleLoade
 
     public abstract ReadableDataSource<String, List<ParamFlowRule>> getParamFlowRuleDataSource();
 
+    // 远程配置中心的规则装载为空或者时候，需要从本地规则文件中装载
+    // 不同的Sentinel DataSource机制不一样，例如：ApolloDataSource默认会装载本地规则文件，NacosDataSource则不会，需要手工装载
     public abstract boolean isFileManualLoaded();
 }
