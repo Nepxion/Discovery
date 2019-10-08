63a2cf88ed8aa63483a5b952d22768591bcd5d78
Nepxion <1394997@qq.com>
2019-10-08 12:40:56 +0800

    重构类结构

---
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/adapter/DefaultRestTemplateStrategyInterceptorAdapter.java
@@ -0,0 +1,31 @@
+package com.nepxion.discovery.plugin.strategy.service.adapter;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
+import com.nepxion.discovery.plugin.strategy.service.context.ServiceStrategyContextHolder;
+
+public abstract class DefaultRestTemplateStrategyInterceptorAdapter implements RestTemplateStrategyInterceptorAdapter {
+    @Autowired
+    protected PluginAdapter pluginAdapter;
+
+    @Autowired
+    protected ServiceStrategyContextHolder serviceStrategyContextHolder;
+
+    public PluginAdapter getPluginAdapter() {
+        return pluginAdapter;
+    }
+
+    public ServiceStrategyContextHolder getServiceStrategyContextHolder() {
+        return serviceStrategyContextHolder;
+    }
+}
