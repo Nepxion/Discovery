ec07a79ce39a5cb3cdfde662c8f2c200d6c04e55
Nepxion <1394997@qq.com>
2019-10-18 12:06:12 +0800

    优化随机权重时候，权重配置全部为0的情况下，抛出的异常信息

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
@@ -58,7 +58,7 @@ public abstract class AbstractMapWeightRandomLoadBalance<T> implements WeightRan
 
         public K random() {
             if (MapUtils.isEmpty(weightMap)) {
-                throw new DiscoveryException("Weights are all <= 0, failed to execute weight load balance");
+                throw new DiscoveryException("Weight values are all <= 0 or invalid format");
             }
 
             double randomWeight = weightMap.lastKey() * Math.random();
