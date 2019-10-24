94cafe10fea77b32ef733ea00ee01694d6ab0739
Nepxion <1394997@qq.com>
2019-10-24 21:50:03 +0800

    增加Opentracing功能

---
+++ discovery-plugin-strategy-opentracing/src/main/java/com/nepxion/discovery/plugin/opentracing/constant/StrategyOpentracingConstant.java
@@ -0,0 +1,14 @@
+package com.nepxion.discovery.plugin.opentracing.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public class StrategyOpentracingConstant {
+    public static final String SPRING_APPLICATION_STRATEGY_TRACE_OPENTRACING_ENABLED = "spring.application.strategy.trace.opentracing.enabled";
+}
