f3b0e201ad9477561651c154e2ce46d519266ef4
Nepxion <1394997@qq.com>
2019-10-25 14:17:20 +0800

    支持过滤器中清除调用链上下文

---
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/filter/ZuulStrategyClearFilter.java
@@ -0,0 +1,17 @@
+package com.nepxion.discovery.plugin.strategy.zuul.filter;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import com.netflix.zuul.IZuulFilter;
+import com.netflix.zuul.ZuulFilter;
+
+public interface ZuulStrategyClearFilter extends IZuulFilter, Comparable<ZuulFilter> {
+
+}
