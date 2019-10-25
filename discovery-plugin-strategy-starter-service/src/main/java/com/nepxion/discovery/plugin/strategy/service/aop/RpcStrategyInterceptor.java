54cf2663e51ebc1e72d14ab2c025cb7c3ef33c96
Nepxion <1394997@qq.com>
2019-10-25 08:00:23 +0800

    去掉日志打印

--- discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/aop/RpcStrategyInterceptor.java
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/aop/RpcStrategyInterceptor.java
@@ -14,16 +14,12 @@ import java.util.Map;
 
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.lang3.ArrayUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.nepxion.discovery.plugin.strategy.service.constant.ServiceStrategyConstant;
 import com.nepxion.discovery.plugin.strategy.service.context.RpcStrategyContext;
 import com.nepxion.matrix.proxy.aop.AbstractInterceptor;
 
 public class RpcStrategyInterceptor extends AbstractInterceptor {
-    private static final Logger LOG = LoggerFactory.getLogger(RpcStrategyInterceptor.class);
-
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Class<?> proxiedClass = getProxiedClass(invocation);
@@ -51,14 +47,10 @@ public class RpcStrategyInterceptor extends AbstractInterceptor {
         context.add(ServiceStrategyConstant.METHOD, methodName);
         context.add(ServiceStrategyConstant.PARAMETER_MAP, parameterMap);
 
-        LOG.debug("Rpc strategy context is set with {}", context);
-
         try {
             return invocation.proceed();
         } finally {
             RpcStrategyContext.clearCurrentContext();
-
-            LOG.debug("Rpc strategy context is cleared");
         }
     }
 }
