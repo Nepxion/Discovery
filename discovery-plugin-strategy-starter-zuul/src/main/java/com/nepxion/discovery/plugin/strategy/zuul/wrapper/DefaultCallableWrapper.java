13d9dd6dc4fbf15dd0e3cba84fc4835226a59b13
Nepxion <1394997@qq.com>
2019-11-06 20:59:43 +0800

    增加调用链对Hystrix的支持

--- discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/wrapper/DefaultCallableWrapper.java
+++ discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/wrapper/DefaultCallableWrapper.java
@@ -15,15 +15,23 @@ import java.util.concurrent.Callable;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.discovery.plugin.strategy.tracer.StrategyTracerContextManager;
 import com.nepxion.discovery.plugin.strategy.wrapper.CallableWrapper;
 import com.nepxion.discovery.plugin.strategy.zuul.context.ZuulStrategyContext;
 import com.netflix.zuul.context.RequestContext;
 
 public class DefaultCallableWrapper implements CallableWrapper {
+    @Autowired(required = false)
+    private StrategyTracerContextManager strategyTracerContextManager;
+
     @Override
     public <T> Callable<T> wrapCallable(Callable<T> delegate) {
         HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
         Map<String, String> headers = RequestContext.getCurrentContext().getZuulRequestHeaders();
+
+        Object tracerContext = getTracerContext();
 
         return new Callable<T>() {
             @Override
@@ -32,11 +40,39 @@ public class DefaultCallableWrapper implements CallableWrapper {
                     ZuulStrategyContext.getCurrentContext().setRequest(request);
                     ZuulStrategyContext.getCurrentContext().setHeaders(headers);
 
+                    setTracerContext(tracerContext);
+
                     return delegate.call();
                 } finally {
                     ZuulStrategyContext.clearCurrentContext();
+
+                    clearTracerContext();
                 }
             }
         };
+    }
+
+    private Object getTracerContext() {
+        if (strategyTracerContextManager == null) {
+            return null;
+        }
+
+        return strategyTracerContextManager.getContext();
+    }
+
+    private void setTracerContext(Object context) {
+        if (strategyTracerContextManager == null) {
+            return;
+        }
+
+        strategyTracerContextManager.setContext(context);
+    }
+
+    private void clearTracerContext() {
+        if (strategyTracerContextManager == null) {
+            return;
+        }
+
+        strategyTracerContextManager.clearContext();
     }
 }
