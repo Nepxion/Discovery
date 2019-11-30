f3ad5aeb8dd377fc32dc0c277750727cedb73a5e
Nepxion <1394997@qq.com>
2019-11-30 22:18:57 +0800

    提交Sentinel Opentracing

---
+++ discovery-plugin-strategy-sentinel-starter-opentracing/src/main/java/com/nepxion/discovery/plugin/strategy/sentinel/opentracing/constant/SentinelOpenTracingConstant.java
@@ -0,0 +1,27 @@
+package com.nepxion.discovery.plugin.strategy.sentinel.opentracing.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author zhang shun
+ * @version 1.0
+ */
+
+public class SentinelOpenTracingConstant {
+    public static final String SPAN_NAME = "SENTINEL";
+    public static final String CONTEXT_NAME = "context.name";
+    public static final String ORIGIN = "origin";
+    public static final String ASYNC = "async";
+    public static final String RESOURCE_NAME = "resource.name";
+    public static final String RESOURCE_SHOW_NAME = "resource.showname";
+    public static final String RESOURCE_TYPE = "resource.type";
+    public static final String ENTRY_TYPE = "entry.type";
+    public static final String RULE_LIMIT_APP = "rule.limit.app";
+    public static final String RULE = "rule";
+    public static final String CAUSE = "cause";
+    public static final String BLOCK_EXCEPTION = "block.exception";
+    public static final String COUNT = "count";
+    public static final String ARGS = "args";
+}
