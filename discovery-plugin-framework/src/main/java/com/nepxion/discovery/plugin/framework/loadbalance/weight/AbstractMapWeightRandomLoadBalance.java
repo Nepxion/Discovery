4ed30845fdaff32ff120be7cc58b834fa484257f
HaojunRen <1394997@qq.com>
2019-12-28 13:11:23 +0800

    增加自定义全链路随机权重

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractMapWeightRandomLoadBalance.java
@@ -11,15 +11,11 @@ package com.nepxion.discovery.plugin.framework.loadbalance.weight;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-import com.nepxion.discovery.common.exception.DiscoveryException;
 import com.nepxion.discovery.plugin.framework.loadbalance.WeightRandomLoadBalance;
 import com.netflix.loadbalancer.Server;
 
@@ -39,32 +35,5 @@ public abstract class AbstractMapWeightRandomLoadBalance<T> implements WeightRan
         MapWeightRandom<Server, Integer> weightRandom = new MapWeightRandom<Server, Integer>(weightPairList);
 
         return weightRandom.random();
-    }
-
-    public class MapWeightRandom<K, V extends Number> {
-        private TreeMap<Double, K> weightMap = new TreeMap<Double, K>();
-
-        public MapWeightRandom(List<Pair<K, V>> pairlist) {
-            for (Pair<K, V> pair : pairlist) {
-                double value = pair.getValue().doubleValue();
-                if (value <= 0) {
-                    continue;
-                }
-
-                double lastWeight = weightMap.size() == 0 ? 0 : weightMap.lastKey().doubleValue();
-                weightMap.put(value + lastWeight, pair.getKey());
-            }
-        }
-
-        public K random() {
-            if (MapUtils.isEmpty(weightMap)) {
-                throw new DiscoveryException("Weight values are all <= 0 or invalid format");
-            }
-
-            double randomWeight = weightMap.lastKey() * Math.random();
-            SortedMap<Double, K> tailMap = weightMap.tailMap(randomWeight, false);
-
-            return weightMap.get(tailMap.firstKey());
-        }
     }
 }
