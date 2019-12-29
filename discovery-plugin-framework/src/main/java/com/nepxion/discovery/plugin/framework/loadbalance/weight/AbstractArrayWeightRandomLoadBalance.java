cc894d93cb8629828a71bd709357b89c8195c923
HaojunRen <1394997@qq.com>
2019-12-29 16:42:07 +0800

    增加自定义全链路随机权重

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractArrayWeightRandomLoadBalance.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/AbstractArrayWeightRandomLoadBalance.java
@@ -10,7 +10,6 @@ package com.nepxion.discovery.plugin.framework.loadbalance.weight;
  */
 
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -18,6 +17,8 @@ import com.nepxion.discovery.plugin.framework.loadbalance.WeightRandomLoadBalanc
 import com.netflix.loadbalancer.Server;
 
 public abstract class AbstractArrayWeightRandomLoadBalance<T> implements WeightRandomLoadBalance<T> {
+    private ArrayWeightRandom arrayWeightRandom = new ArrayWeightRandom();
+
     @Override
     public Server choose(List<Server> serverList, T t) {
         if (CollectionUtils.isEmpty(serverList)) {
@@ -30,36 +31,8 @@ public abstract class AbstractArrayWeightRandomLoadBalance<T> implements WeightR
             weights[i] = getWeight(server, t);
         }
 
-        int index = getIndex(weights);
+        int index = arrayWeightRandom.getIndex(weights);
 
         return serverList.get(index);
-    }
-
-    private int getIndex(int[] weights) {
-        // 次序号/权重区间值
-        int[][] weightHolder = new int[weights.length][2];
-        // 总权重
-        int totalWeight = 0;
-        // 赋值次序号和区间值累加的数组值，从小到大排列
-        // 例如，对于权重分别为20，40， 60的三个服务，将形成[0, 20)，[20, 60)，[60, 120]三个区间
-        for (int i = 0; i < weights.length; i++) {
-            if (weights[i] <= 0) {
-                continue;
-            }
-
-            totalWeight += weights[i];
-            weightHolder[i][0] = i;
-            weightHolder[i][1] = totalWeight;
-        }
-
-        // 获取介于0(含)和n(不含)伪随机，均匀分布的int值
-        int hitWeight = ThreadLocalRandom.current().nextInt(totalWeight) + 1; // [1, totalWeight)
-        for (int i = 0; i < weightHolder.length; i++) {
-            if (hitWeight <= weightHolder[i][1]) {
-                return weightHolder[i][0];
-            }
-        }
-
-        return weightHolder[0][0];
     }
 }
