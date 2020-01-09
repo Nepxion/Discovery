71b6b7d8017e51c7602c190dfad83f1820fbe914
HaojunRen <1394997@qq.com>
2020-01-09 18:24:48 +0800

    Bump version to 4.13.12

--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=4.13.11
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=4.13.12
 
 pause
