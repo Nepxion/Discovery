601fa2d3a6fb7f585288bafab879f40bf94230ab
HaojunRen <1394997@qq.com>
2020-03-22 07:40:14 +0800

    增加服务具备像网关那样做灰度蓝绿功能的策源地

--- discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/filter/DefaultServiceStrategyRouteFilter.java
+++ discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/filter/DefaultServiceStrategyRouteFilter.java
@@ -9,36 +9,85 @@ package com.nepxion.discovery.plugin.strategy.service.filter;
  * @version 1.0
  */
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.nepxion.discovery.common.entity.RuleEntity;
+import com.nepxion.discovery.common.entity.StrategyCustomizationEntity;
+import com.nepxion.discovery.common.entity.StrategyHeaderEntity;
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
 import com.nepxion.discovery.plugin.strategy.wrapper.StrategyWrapper;
 
 public class DefaultServiceStrategyRouteFilter extends AbstractServiceStrategyRouteFilter {
+    @Autowired
+    private PluginAdapter pluginAdapter;
+
     @Autowired
     private StrategyWrapper strategyWrapper;
 
     @Override
     public String getRouteVersion() {
-        return strategyWrapper.getRouteVersion();
+        Map<String, String> headerMap = getHeaderMap();
+        if (headerMap == null) {
+            return strategyWrapper.getRouteVersion();
+        } else {
+            return strategyWrapper.getRouteVersion(headerMap);
+        }
     }
 
     @Override
     public String getRouteRegion() {
-        return strategyWrapper.getRouteRegion();
+        Map<String, String> headerMap = getHeaderMap();
+        if (headerMap == null) {
+            return strategyWrapper.getRouteRegion();
+        } else {
+            return strategyWrapper.getRouteRegion(headerMap);
+        }
     }
 
     @Override
     public String getRouteAddress() {
-        return strategyWrapper.getRouteAddress();
+        Map<String, String> headerMap = getHeaderMap();
+        if (headerMap == null) {
+            return strategyWrapper.getRouteAddress();
+        } else {
+            return strategyWrapper.getRouteAddress(headerMap);
+        }
     }
 
     @Override
     public String getRouteVersionWeight() {
-        return strategyWrapper.getRouteVersionWeight();
+        Map<String, String> headerMap = getHeaderMap();
+        if (headerMap == null) {
+            return strategyWrapper.getRouteVersionWeight();
+        } else {
+            return strategyWrapper.getRouteVersionWeight(headerMap);
+        }
     }
 
     @Override
     public String getRouteRegionWeight() {
-        return strategyWrapper.getRouteRegionWeight();
+        Map<String, String> headerMap = getHeaderMap();
+        if (headerMap == null) {
+            return strategyWrapper.getRouteRegionWeight();
+        } else {
+            return strategyWrapper.getRouteRegionWeight(headerMap);
+        }
+    }
+
+    public Map<String, String> getHeaderMap() {
+        RuleEntity ruleEntity = pluginAdapter.getRule();
+        if (ruleEntity != null) {
+            StrategyCustomizationEntity strategyCustomizationEntity = ruleEntity.getStrategyCustomizationEntity();
+            if (strategyCustomizationEntity != null) {
+                StrategyHeaderEntity strategyHeaderEntity = strategyCustomizationEntity.getStrategyHeaderEntity();
+                if (strategyHeaderEntity != null) {
+                    return strategyHeaderEntity.getHeaderMap();
+                }
+            }
+        }
+
+        return null;
     }
 }
