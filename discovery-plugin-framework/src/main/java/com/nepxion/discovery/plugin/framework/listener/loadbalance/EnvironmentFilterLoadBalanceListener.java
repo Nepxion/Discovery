6c24949d6fecdb6fbdbafec27b4093ed6a43359b
HaojunRen <1394997@qq.com>
2020-06-22 10:22:02 +0800

    更正注释

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/listener/loadbalance/EnvironmentFilterLoadBalanceListener.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/listener/loadbalance/EnvironmentFilterLoadBalanceListener.java
@@ -72,7 +72,7 @@ public class EnvironmentFilterLoadBalanceListener extends AbstractLoadBalanceLis
 
     @Override
     public int getOrder() {
-        // Highest priority
+        // After region filter
         return HIGHEST_PRECEDENCE + 3;
     }
 }
