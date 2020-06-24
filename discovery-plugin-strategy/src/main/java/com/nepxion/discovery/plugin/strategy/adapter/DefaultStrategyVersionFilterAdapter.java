3afd11a7e4c0156be85012655c5324cf1ca3816e
HaojunRen <1394997@qq.com>
2020-06-24 14:08:15 +0800

    策略模式下版本过滤

--- discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/adapter/DefaultStrategyVersionFilterAdapter.java
+++ discovery-plugin-strategy/src/main/java/com/nepxion/discovery/plugin/strategy/adapter/DefaultStrategyVersionFilterAdapter.java
@@ -9,19 +9,15 @@ package com.nepxion.discovery.plugin.strategy.adapter;
  * @version 1.0
  */
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.collections4.CollectionUtils;
 
 public class DefaultStrategyVersionFilterAdapter implements StrategyVersionFilterAdapter {
     @Override
     public List<String> filter(List<String> versionList) {
-        List<String> filterVersionList = new ArrayList<String>();
-        if (CollectionUtils.isNotEmpty(versionList)) {
-            filterVersionList.add(versionList.get(0));
-        }
+        String version = versionList.get(0);
+        versionList.clear();
+        versionList.add(version);
 
-        return filterVersionList;
+        return versionList;
     }
 }
