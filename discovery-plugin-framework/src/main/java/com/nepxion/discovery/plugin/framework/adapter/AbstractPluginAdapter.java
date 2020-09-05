675f5bc650c19e04c3045eef4a58a5728fee7d9e
HaojunRen <1394997@qq.com>
2020-09-05 19:33:30 +0800

    修改介绍

--- discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/adapter/AbstractPluginAdapter.java
+++ discovery-plugin-framework/src/main/java/com/nepxion/discovery/plugin/framework/adapter/AbstractPluginAdapter.java
@@ -113,7 +113,7 @@ public abstract class AbstractPluginAdapter implements PluginAdapter {
 
     @Override
     public Map<String, String> getMetadata() {
-        // 获取本服务静态元数据，当元数据变更后，本服务是不会更新的
+        // 获取本服务静态元数据，当元数据通过Open API方式变更后，本服务是不会更新的
         return registration.getMetadata();
     }
 
