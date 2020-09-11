034de0ce634f68d8bd83a271819bfa20ec170d7d
HaojunRen <1394997@qq.com>
2020-09-11 09:13:59 +0800

    增加应用类型

--- discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/context/GatewayStrategyApplicationContextInitializer.java
+++ discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/context/GatewayStrategyApplicationContextInitializer.java
@@ -13,10 +13,11 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.entity.ApplicationType;
 
 public class GatewayStrategyApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        System.setProperty(DiscoveryConstant.SPRING_APPLICATION_TYPE, DiscoveryConstant.GATEWAY_TYPE);
+        System.setProperty(DiscoveryConstant.SPRING_APPLICATION_TYPE, ApplicationType.GATEWAY.toString());
     }
 }
