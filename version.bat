f19ca5171aee710969d91fdad58e67efb21df0b4
HaojunRen <1394997@qq.com>
2020-09-16 08:34:29 +0800

    Bump version to 3.18.0-SNAPSHOT

--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=3.17.0
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=3.18.0-SNAPSHOT
 
 pause
