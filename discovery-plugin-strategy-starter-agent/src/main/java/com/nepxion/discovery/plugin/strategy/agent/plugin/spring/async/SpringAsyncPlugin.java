a7d1c12cd0458c31002996f676bf71bf163809bb
echo <zifeihan@foxmail.com>
2020-09-18 11:53:48 +0800

    支持Spring @Async注解

---
+++ discovery-plugin-strategy-starter-agent/src/main/java/com/nepxion/discovery/plugin/strategy/agent/plugin/spring/async/SpringAsyncPlugin.java
@@ -0,0 +1,56 @@
+package com.nepxion.discovery.plugin.strategy.agent.plugin.spring.async;
+
+import com.nepxion.discovery.plugin.strategy.agent.callback.TransformCallback;
+import com.nepxion.discovery.plugin.strategy.agent.callback.TransformTemplate;
+import com.nepxion.discovery.plugin.strategy.agent.matcher.ClassMatcher;
+import com.nepxion.discovery.plugin.strategy.agent.matcher.MatcherFactory;
+import com.nepxion.discovery.plugin.strategy.agent.plugin.Plugin;
+import com.nepxion.discovery.plugin.strategy.agent.util.ClassInfo;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+import java.security.ProtectionDomain;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author zifeihan
+ * @version 1.0
+ */
+
+public class SpringAsyncPlugin extends Plugin {
+    @Override
+    public void install(TransformTemplate transformTemplate) {
+        ClassMatcher classNameMatcher = MatcherFactory.newClassNameMatcher("org.springframework.aop.interceptor.AsyncExecutionAspectSupport");
+        transformTemplate.transform(classNameMatcher, new TransformCallback() {
+            @Override
+            public byte[] doInTransform(ClassLoader classLoader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+
+                try {
+                    ClassInfo classInfo = new ClassInfo(className, classfileBuffer, classLoader);
+                    CtClass ctClass = classInfo.getCtClass();
+                    CtMethod method = ctClass.getMethod("doSubmit", "(Ljava/util/concurrent/Callable;Lorg/springframework/core/task/AsyncTaskExecutor;Ljava/lang/Class;)Ljava/lang/Object;");
+                    if (null != method) {
+                        String code = "";
+                        CtClass[] parameterTypes = method.getParameterTypes();
+                        for (int i = 0; i < parameterTypes.length; i++) {
+                            final String paramTypeName = parameterTypes[i].getName();
+                            if ("java.util.concurrent.Callable".equals(paramTypeName)) {
+                                code += String.format("$%d = new com.nepxion.discovery.plugin.strategy.agent.plugin.spring.async.WrapCallable($%d);", i + 1, i + 1);
+                            }
+                        }
+
+                        if (code.length() > 0) {
+                            method.insertBefore(code);
+                        }
+                    }
+                    return ctClass.toBytecode();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        });
+    }
+}
