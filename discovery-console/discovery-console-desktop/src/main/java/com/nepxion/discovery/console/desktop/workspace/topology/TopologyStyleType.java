db4ce8bc921b59f76e3aa5f0ad0994a608407b12
HaojunRen <1394997@qq.com>
2020-09-25 20:50:55 +0800

    重构Maven结构

---
+++ discovery-console/discovery-console-desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/topology/TopologyStyleType.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.console.desktop.workspace.topology;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public enum TopologyStyleType {
+    LARGE,
+    MIDDLE,
+    SMALL
+}
