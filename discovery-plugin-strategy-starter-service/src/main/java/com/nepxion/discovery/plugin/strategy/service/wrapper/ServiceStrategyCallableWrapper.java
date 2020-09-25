201d8c915f5790ccc9a94848a030522280b7bbf4
HaojunRen <1394997@qq.com>
2020-09-25 21:47:17 +0800

    重构Maven结构

--- discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/wrapper/ServiceStrategyCallableWrapper.java
+++
@@ -1,7 +0,0 @@
-package com.nepxion.discovery.plugin.strategy.service.wrapper;
-
-import com.nepxion.discovery.plugin.strategy.wrapper.StrategyCallableWrapper;
-
-public interface ServiceStrategyCallableWrapper extends StrategyCallableWrapper {
-
-}
