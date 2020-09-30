eb39485a9c9d6c3ded215fe8f445ac1a9733af13
HaojunRen <1394997@qq.com>
2020-09-30 19:06:03 +0800

    Bump version to 3.20.1

--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=3.20.0
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=3.20.1
 
 pause
