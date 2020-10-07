58959b61a48da62ca135ba6b6c96409dc8f9e237
HaojunRen <1394997@qq.com>
2020-10-07 15:32:49 +0800

    支持过滤器中自定义环境隔离策略

--- discovery-plugin-strategy/discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/filter/DefaultGatewayStrategyRouteFilter.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter-gateway/src/main/java/com/nepxion/discovery/plugin/strategy/gateway/filter/DefaultGatewayStrategyRouteFilter.java
@@ -28,6 +28,11 @@ public class DefaultGatewayStrategyRouteFilter extends AbstractGatewayStrategyRo
     }
 
     @Override
+    public String getRouteEnvironment() {
+        return strategyContextHolder.getRouteEnvironment();
+    }
+
+    @Override
     public String getRouteAddress() {
         return strategyContextHolder.getRouteAddress();
     }
