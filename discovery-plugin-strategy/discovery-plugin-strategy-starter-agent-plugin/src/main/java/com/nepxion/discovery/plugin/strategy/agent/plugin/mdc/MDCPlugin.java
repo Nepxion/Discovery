0fa271b22cdcf86ffb651acbd5cce5b24733a062
HaojunRen <1394997@qq.com>
2020-10-15 16:07:59 +0800

    增加SLF4J MDC异步Agent

---
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter-agent-plugin/src/main/java/com/nepxion/discovery/plugin/strategy/agent/plugin/mdc/MDCPlugin.java
@@ -0,0 +1,24 @@
+package com.nepxion.discovery.plugin.strategy.agent.plugin.mdc;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author HaojunRen
+ * @version 1.0
+ */
+
+import com.nepxion.discovery.plugin.strategy.agent.plugin.AbstractPlugin;
+
+public class MDCPlugin extends AbstractPlugin {
+    @Override
+    protected String getMatcherClassName() {
+        return "org.slf4j.MDC";
+    }
+
+    @Override
+    protected String getHookClassName() {
+        return MDCContextHook.class.getName();
+    }
+}
