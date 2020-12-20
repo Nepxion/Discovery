b27bf19c739697ca8178aa80cacd653a3ce1168f
HaojunRen <1394997@qq.com>
2020-12-21 00:25:00 +0800

    Bump version to 3.21.0

--- discovery-commons/discovery-common-zookeeper/src/main/java/com/nepxion/discovery/common/zookeeper/constant/ZookeeperConstant.java
+++ discovery-commons/discovery-common-zookeeper/src/main/java/com/nepxion/discovery/common/zookeeper/constant/ZookeeperConstant.java
@@ -12,7 +12,7 @@ package com.nepxion.discovery.common.zookeeper.constant;
 public class ZookeeperConstant {
     public static final String ZOOKEEPER_TYPE = "Zookeeper";
     public static final String ZOOKEEPER_CONNECT_STRING = "zookeeper.connect-string";
-    public static final String ZOOKEEPER_RETRY_COUNT = "zookeeper.retry.count";
+    public static final String ZOOKEEPER_RETRY_COUNT = "zookeeper.retry-count";
     public static final String ZOOKEEPER_SLEEP_TIME = "zookeeper.sleep-time";
     public static final String ZOOKEEPER_DEFAULT_RETRY_COUNT_VALUE = "3";
     public static final String ZOOKEEPER_DEFAULT_SLEEP_TIME_VALUE = "6000";
