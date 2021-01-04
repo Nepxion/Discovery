de48e3ea3a6298fc8bd010b1ccaa839cfe58381d
HaojunRen <1394997@qq.com>
2021-01-04 22:47:51 +0800

    Add LoadBalancer Banner

--- discovery-plugin-framework/discovery-plugin-framework-starter/src/main/java/com/nepxion/discovery/plugin/framework/configuration/PluginAutoConfiguration.java
+++ discovery-plugin-framework/discovery-plugin-framework-starter/src/main/java/com/nepxion/discovery/plugin/framework/configuration/PluginAutoConfiguration.java
@@ -16,6 +16,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
 import com.nepxion.discovery.plugin.framework.cache.PluginCache;
@@ -42,10 +46,17 @@ import com.nepxion.discovery.plugin.framework.listener.register.RegisterListener
 import com.nepxion.discovery.plugin.framework.loadbalance.weight.RuleMapWeightRandomLoadBalance;
 import com.nepxion.discovery.plugin.framework.loadbalance.weight.StrategyMapWeightRandomLoadBalance;
 import com.nepxion.eventbus.annotation.EnableEventBus;
+import com.taobao.text.Color;
 
 @Configuration
 @EnableEventBus
 public class PluginAutoConfiguration {
+    static {
+        LogoBanner logoBanner = new LogoBanner(PluginAutoConfiguration.class, "/com/nepxion/springcloudloadBalancer/resource/logo.txt", "Welcome to Nepxion", 4, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue }, true);
+
+        NepxionBanner.show(logoBanner, new Description("LoadBalancer:", "Spring Cloud LoadBalancer", 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Discovery", 0, 1));
+    }
+
     @Bean
     @LoadBalanced
     public RestTemplate pluginRestTemplate() {
