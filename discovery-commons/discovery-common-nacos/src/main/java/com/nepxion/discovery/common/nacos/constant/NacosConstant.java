45b71410688264faddfd8c77bc65bc305d2776f5
HaojunRen <1394997@qq.com>
2021-01-28 13:06:22 +0800

    Bump version to 3.22.0

--- discovery-commons/discovery-common-nacos/src/main/java/com/nepxion/discovery/common/nacos/constant/NacosConstant.java
+++ discovery-commons/discovery-common-nacos/src/main/java/com/nepxion/discovery/common/nacos/constant/NacosConstant.java
@@ -38,4 +38,6 @@ public class NacosConstant extends PropertyKeyConst {
     public static final String NACOS_PLUGIN_ENABLE_REMOTE_SYNC_CONFIG = "nacos.plugin.enable-remote-sync-config";
     public static final String NACOS_PLUGIN_TIMEOUT = "nacos.plugin.timout";
     public static final long NACOS_PLUGIN_DEFAULT_TIMEOUT = 30000;
+
+    public static final String NACOS_PLUGIN_PREFIX = "nacos.plugin";
 }
