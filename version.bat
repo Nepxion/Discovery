23aca723233693cb6eb73f837adafa9a2532f312
HaojunRen <1394997@qq.com>
2021-03-24 18:34:43 +0800

    Bump version to 6.8.0

--- version.bat
+++ version.bat
@@ -15,5 +15,5 @@
 @title Nepxion Discovery
 @color 0a
 
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=6.8.0-SNAPSHOT
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=6.8.0
 pause
