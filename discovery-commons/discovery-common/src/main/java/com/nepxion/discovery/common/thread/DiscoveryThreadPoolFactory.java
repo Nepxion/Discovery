451e9f26de6dded2860454b5ab958352ec2dc8d6
HaojunRen <1394997@qq.com>
2021-05-01 18:05:30 +0800

    Optimize Nacos and Consul config ThreadPool

--- discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/thread/DiscoveryThreadPoolFactory.java
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/thread/DiscoveryThreadPoolFactory.java
@@ -9,23 +9,13 @@ package com.nepxion.discovery.common.thread;
  * @version 1.0
  */
 
-import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class DiscoveryThreadPoolFactory {
-    private static Map<String, ExecutorService> executorServiceMap = new ConcurrentHashMap<String, ExecutorService>();
-
     public static ExecutorService getExecutorService(String name) {
-        ExecutorService executorService = executorServiceMap.get(name);
-        if (executorService == null) {
-            executorService = new ThreadPoolExecutor(2, 4, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(1), new DiscoveryNamedThreadFactory(name), new ThreadPoolExecutor.DiscardOldestPolicy());
-            executorServiceMap.put(name, executorService);
-        }
-
-        return executorService;
+        return new ThreadPoolExecutor(2, 4, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(1), new DiscoveryNamedThreadFactory(name), new ThreadPoolExecutor.DiscardOldestPolicy());
     }
 }
