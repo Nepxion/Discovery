1f6a16a08f0b28b82b5fe59277a9d1201d38ff55
HaojunRen <1394997@qq.com>
2021-05-06 21:27:37 +0800

    Refactory endpoints

---
+++ discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/resource/ConfigResourceImpl.java
@@ -0,0 +1,95 @@
+package com.nepxion.discovery.plugin.admincenter.resource;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nepxion.discovery.common.entity.RuleEntity;
+import com.nepxion.discovery.common.entity.SubscriptionType;
+import com.nepxion.discovery.common.exception.DiscoveryException;
+import com.nepxion.discovery.plugin.framework.adapter.PluginAdapter;
+import com.nepxion.discovery.plugin.framework.context.PluginContextAware;
+import com.nepxion.discovery.plugin.framework.event.PluginEventWapper;
+import com.nepxion.discovery.plugin.framework.event.RuleClearedEvent;
+import com.nepxion.discovery.plugin.framework.event.RuleUpdatedEvent;
+
+public class ConfigResourceImpl implements ConfigResource {
+    @Autowired
+    private PluginContextAware pluginContextAware;
+
+    @Autowired
+    private PluginAdapter pluginAdapter;
+
+    @Autowired
+    private PluginEventWapper pluginEventWapper;
+
+    @Override
+    public void update(String config, boolean async) {
+        Boolean discoveryControlEnabled = pluginContextAware.isDiscoveryControlEnabled();
+        if (!discoveryControlEnabled) {
+            throw new DiscoveryException("Discovery control is disabled");
+        }
+
+        Boolean isConfigRestControlEnabled = pluginContextAware.isConfigRestControlEnabled();
+        if (!isConfigRestControlEnabled) {
+            throw new DiscoveryException("Config rest control is disabled");
+        }
+
+        pluginEventWapper.fireRuleUpdated(new RuleUpdatedEvent(SubscriptionType.PARTIAL, config), async);
+    }
+
+    @Override
+    public void clear(boolean async) {
+        Boolean discoveryControlEnabled = pluginContextAware.isDiscoveryControlEnabled();
+        if (!discoveryControlEnabled) {
+            throw new DiscoveryException("Discovery control is disabled");
+        }
+
+        Boolean isConfigRestControlEnabled = pluginContextAware.isConfigRestControlEnabled();
+        if (!isConfigRestControlEnabled) {
+            throw new DiscoveryException("Config rest control is disabled");
+        }
+
+        pluginEventWapper.fireRuleCleared(new RuleClearedEvent(SubscriptionType.PARTIAL), async);
+    }
+
+    @Override
+    public List<String> view() {
+        List<String> ruleList = new ArrayList<String>(3);
+
+        String localRuleContent = StringUtils.EMPTY;
+        RuleEntity localRuleEntity = pluginAdapter.getLocalRule();
+        if (localRuleEntity != null && StringUtils.isNotEmpty(localRuleEntity.getContent())) {
+            localRuleContent = localRuleEntity.getContent();
+        }
+
+        String dynamicGlobalRuleContent = StringUtils.EMPTY;
+        RuleEntity dynamicGlobalRuleEntity = pluginAdapter.getDynamicGlobalRule();
+        if (dynamicGlobalRuleEntity != null && StringUtils.isNotEmpty(dynamicGlobalRuleEntity.getContent())) {
+            dynamicGlobalRuleContent = dynamicGlobalRuleEntity.getContent();
+        }
+
+        String dynamicPartialRuleContent = StringUtils.EMPTY;
+        RuleEntity dynamicPartialRuleEntity = pluginAdapter.getDynamicPartialRule();
+        if (dynamicPartialRuleEntity != null && StringUtils.isNotEmpty(dynamicPartialRuleEntity.getContent())) {
+            dynamicPartialRuleContent = dynamicPartialRuleEntity.getContent();
+        }
+
+        ruleList.add(localRuleContent);
+        ruleList.add(dynamicGlobalRuleContent);
+        ruleList.add(dynamicPartialRuleContent);
+
+        return ruleList;
+    }
+}
