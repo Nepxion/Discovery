9d444f5ffcaa479af7275155f915911697bb98d3
HaojunRen <1394997@qq.com>
2021-05-06 23:52:02 +0800

    Refactory endpoints

--- discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/configuration/AdminAutoConfiguration.java
+++ discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/configuration/AdminAutoConfiguration.java
@@ -29,9 +29,33 @@ import com.nepxion.discovery.plugin.admincenter.endpoint.InspectorEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.RouterEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.SentinelCoreEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.SentinelParamEndpoint;
+import com.nepxion.discovery.plugin.admincenter.endpoint.ServiceEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.StrategyEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.VersionEndpoint;
 import com.nepxion.discovery.plugin.admincenter.endpoint.ZuulStrategyRouteEndpoint;
+import com.nepxion.discovery.plugin.admincenter.resource.ConfigResource;
+import com.nepxion.discovery.plugin.admincenter.resource.ConfigResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.GatewayStrategyRouteResource;
+import com.nepxion.discovery.plugin.admincenter.resource.GatewayStrategyRouteResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.GitResource;
+import com.nepxion.discovery.plugin.admincenter.resource.GitResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.InspectorResource;
+import com.nepxion.discovery.plugin.admincenter.resource.InspectorResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.RouterResource;
+import com.nepxion.discovery.plugin.admincenter.resource.RouterResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.SentinelCoreResource;
+import com.nepxion.discovery.plugin.admincenter.resource.SentinelCoreResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.SentinelParamResource;
+import com.nepxion.discovery.plugin.admincenter.resource.SentinelParamResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.ServiceResource;
+import com.nepxion.discovery.plugin.admincenter.resource.ServiceResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.StrategyResource;
+import com.nepxion.discovery.plugin.admincenter.resource.StrategyResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.VersionResource;
+import com.nepxion.discovery.plugin.admincenter.resource.VersionResourceImpl;
+import com.nepxion.discovery.plugin.admincenter.resource.ZuulStrategyRouteResource;
+import com.nepxion.discovery.plugin.admincenter.resource.ZuulStrategyRouteResourceImpl;
+import com.nepxion.discovery.plugin.framework.generator.GitGenerator;
 import com.nepxion.discovery.plugin.strategy.gateway.route.GatewayStrategyRoute;
 import com.nepxion.discovery.plugin.strategy.wrapper.StrategyWrapper;
 import com.nepxion.discovery.plugin.strategy.zuul.route.ZuulStrategyRoute;
@@ -42,8 +66,28 @@ import com.nepxion.discovery.plugin.strategy.zuul.route.ZuulStrategyRoute;
 public class AdminAutoConfiguration {
     protected static class AdminEndpointConfiguration {
         @Bean
+        public ServiceResource serviceResource() {
+            return new ServiceResourceImpl();
+        }
+
+        @Bean
+        public ServiceEndpoint serviceEndpoint() {
+            return new ServiceEndpoint();
+        }
+
+        @Bean
+        public ConfigResource configResource() {
+            return new ConfigResourceImpl();
+        }
+
+        @Bean
         public ConfigEndpoint configEndpoint() {
             return new ConfigEndpoint();
+        }
+
+        @Bean
+        public VersionResource versionResource() {
+            return new VersionResourceImpl();
         }
 
         @Bean
@@ -52,23 +96,33 @@ public class AdminAutoConfiguration {
         }
 
         @Bean
+        public InspectorResource inspectorResource() {
+            return new InspectorResourceImpl();
+        }
+
+        @Bean
         public InspectorEndpoint inspectorEndpoint() {
             return new InspectorEndpoint();
+        }
+
+        @Bean
+        public RouterResource routerResource() {
+            return new RouterResourceImpl();
         }
 
         @Bean
         public RouterEndpoint routerEndpoint() {
             return new RouterEndpoint();
         }
-
-        @Bean
-        public GitEndpoint gitEndpoint() {
-            return new GitEndpoint();
-        }
     }
 
     @ConditionalOnClass(StrategyWrapper.class)
     protected static class StrategyEndpointConfiguration {
+        @Bean
+        public StrategyResource strategyResource() {
+            return new StrategyResourceImpl();
+        }
+
         @Bean
         public StrategyEndpoint strategyEndpoint() {
             return new StrategyEndpoint();
@@ -78,6 +132,11 @@ public class AdminAutoConfiguration {
     @ConditionalOnClass(FlowRule.class)
     protected static class SentinelCoreEndpointConfiguration {
         @Bean
+        public SentinelCoreResource sentinelCoreResource() {
+            return new SentinelCoreResourceImpl();
+        }
+
+        @Bean
         public SentinelCoreEndpoint sentinelCoreEndpoint() {
             return new SentinelCoreEndpoint();
         }
@@ -86,13 +145,37 @@ public class AdminAutoConfiguration {
     @ConditionalOnClass(ParamFlowRule.class)
     protected static class SentinelParamEndpointConfiguration {
         @Bean
+        public SentinelParamResource sentinelParamResource() {
+            return new SentinelParamResourceImpl();
+        }
+
+        @Bean
         public SentinelParamEndpoint sentinelParamEndpoint() {
             return new SentinelParamEndpoint();
         }
     }
 
+    @ConditionalOnBean(GitGenerator.class)
+    protected static class GitEndpointConfiguration {
+        @Bean
+        public GitResource gitResource() {
+            return new GitResourceImpl();
+        }
+
+        @Bean
+        public GitEndpoint gitEndpoint() {
+            return new GitEndpoint();
+        }
+    }
+
     @ConditionalOnBean(GatewayStrategyRoute.class)
     protected static class GatewayStrategyRouteEndpointConfiguration {
+        @Bean
+        @ConditionalOnProperty(value = "spring.cloud.gateway.discovery.locator.enabled", havingValue = "false", matchIfMissing = true)
+        public GatewayStrategyRouteResource gatewayStrategyRouteResource() {
+            return new GatewayStrategyRouteResourceImpl();
+        }
+
         @Bean
         @ConditionalOnProperty(value = "spring.cloud.gateway.discovery.locator.enabled", havingValue = "false", matchIfMissing = true)
         public GatewayStrategyRouteEndpoint gatewayStrategyRouteEndpoint() {
@@ -102,6 +185,11 @@ public class AdminAutoConfiguration {
 
     @ConditionalOnBean(ZuulStrategyRoute.class)
     protected static class ZuulStrategyRouteEndpointConfiguration {
+        @Bean
+        public ZuulStrategyRouteResource zuulStrategyRouteResource() {
+            return new ZuulStrategyRouteResourceImpl();
+        }
+
         @Bean
         public ZuulStrategyRouteEndpoint zuulStrategyRouteEndpoint() {
             return new ZuulStrategyRouteEndpoint();
