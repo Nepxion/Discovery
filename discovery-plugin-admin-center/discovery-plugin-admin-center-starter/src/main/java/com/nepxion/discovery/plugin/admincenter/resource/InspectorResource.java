75dc93539d79c56864cad32effbfdce6f939e781
HaojunRen <1394997@qq.com>
2021-05-06 22:42:10 +0800

    Refactory endpoints

---
+++ discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/resource/InspectorResource.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.plugin.admincenter.resource;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import com.nepxion.discovery.common.entity.InspectorEntity;
+
+public interface InspectorResource {
+    InspectorEntity inspect(InspectorEntity inspectorEntity);
+}
