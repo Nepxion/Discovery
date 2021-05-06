ccbbed3e4ea3af3c1437eed3f37506fcc72cdc56
HaojunRen <1394997@qq.com>
2021-05-06 20:52:14 +0800

    Refactory endpoints

---
+++ discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/resource/ServiceResource.java
@@ -0,0 +1,33 @@
+package com.nepxion.discovery.plugin.admincenter.resource;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.cloud.client.ServiceInstance;
+
+import com.nepxion.discovery.common.entity.InstanceEntity;
+
+public interface ServiceResource {
+    String getDiscoveryType();
+
+    List<String> getGroups();
+
+    List<String> getServices();
+
+    List<ServiceInstance> getInstances(String serviceId);
+
+    List<String> getGateways();
+
+    List<InstanceEntity> getInstanceList(String service);
+
+    Map<String, List<InstanceEntity>> getInstanceMap(List<String> groups);
+}
