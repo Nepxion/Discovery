fa1143f606287a1f614664eb342c7efd4ea74911
HaojunRen <1394997@qq.com>
2021-05-09 10:33:38 +0800

    Add Zuul config processor mechanisms

--- discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/constant/StrategyConstant.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/constant/StrategyConstant.java
@@ -45,7 +45,5 @@ public class StrategyConstant {
     public static final String SPRING_APPLICATION_STRATEGY_AUTO_SCAN_PACKAGES_ENABLED = "spring.application.strategy.auto.scan.packages.enabled";
     public static final String SPRING_APPLICATION_STRATEGY_AUTO_SCAN_RECURSION_ENABLED = "spring.application.strategy.auto.scan.recursion.enabled";
 
-    public static final String SPRING_APPLICATION_STRATEGY_DYNAMIC_ROUTE_GROUP = "spring.application.strategy.dynamic.route.group";
-
     public static final String STRATEGY_ALARM_HEADER="strategy-alarm-header";
 }
