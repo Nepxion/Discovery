203474a8ea8bcdbbe8d07e311df4d89ea18141ad
HaojunRen <1394997@qq.com>
2021-05-09 18:25:55 +0800

    Remove Sentinel extension datasource modules

---
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter-sentinel-datasource/src/main/java/com/nepxion/discovery/plugin/strategy/sentinel/datasource/parser/SentinelStrategyAuthorityRuleParser.java
@@ -0,0 +1,25 @@
+package com.nepxion.discovery.plugin.strategy.sentinel.datasource.parser;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.util.List;
+
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityRule;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+public class SentinelStrategyAuthorityRuleParser implements Converter<String, List<AuthorityRule>> {
+    @Override
+    public List<AuthorityRule> convert(String source) {
+        return JSON.parseObject(source, new TypeReference<List<AuthorityRule>>() {
+        });
+    }
+}
