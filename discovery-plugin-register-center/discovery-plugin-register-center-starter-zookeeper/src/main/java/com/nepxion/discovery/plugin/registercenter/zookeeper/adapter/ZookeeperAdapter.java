559021a32e3b6b6d2f402d5aff42e7f738e36a45
HaojunRen <1394997@qq.com>
2021-05-14 15:15:51 +0800

    Fix Zookeeper discovery metadata bug

--- discovery-plugin-register-center/discovery-plugin-register-center-starter-zookeeper/src/main/java/com/nepxion/discovery/plugin/registercenter/zookeeper/adapter/ZookeeperAdapter.java
+++ discovery-plugin-register-center/discovery-plugin-register-center-starter-zookeeper/src/main/java/com/nepxion/discovery/plugin/registercenter/zookeeper/adapter/ZookeeperAdapter.java
@@ -13,25 +13,14 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.zookeeper.discovery.ZookeeperServer;
 import org.springframework.cloud.zookeeper.serviceregistry.ZookeeperRegistration;
-import org.springframework.core.env.Environment;
 
 import com.nepxion.discovery.common.exception.DiscoveryException;
 import com.nepxion.discovery.plugin.framework.adapter.AbstractPluginAdapter;
-import com.nepxion.discovery.plugin.registercenter.zookeeper.constant.ZookeeperConstant;
 import com.netflix.loadbalancer.Server;
 
 public class ZookeeperAdapter extends AbstractPluginAdapter {
-    @Autowired
-    private Environment environment;
-
-    private String group;
-
-    /*private AtomicBoolean flag = new AtomicBoolean(false);*/
-    private boolean flag = false;
-
     // Zookeeper比较特殊，getServiceInstance是Lazy Initialize模式
     @PostConstruct
     private void initialize() {
@@ -44,22 +33,6 @@ public class ZookeeperAdapter extends AbstractPluginAdapter {
         }
 
         throw new DiscoveryException("Registration isn't the type of ZookeeperRegistration");
-    }
-
-    // Zookeeper比较特殊，父类中getMetadata().get(groupKey)方法不行，执行该方法的时候Metadata还没初始化
-    @Override
-    protected String getGroup(String groupKey) {
-        /*if (flag.compareAndSet(false, true)) {
-            group = environment.getProperty(ZookeeperConstant.ZOOKEEPER_METADATA + "." + groupKey);
-        }*/
-
-        if (!flag) {
-            group = environment.getProperty(ZookeeperConstant.ZOOKEEPER_METADATA + "." + groupKey);
-
-            flag = true;
-        }
-
-        return group;
     }
 
     @Override
