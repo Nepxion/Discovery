b965b00eba1416f3079afc71f40defd12da482fa
HaojunRen <1394997@qq.com>
2021-05-30 09:46:24 +0800

    Move GatewayStrategyRouteEntity to common module

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/entity/GatewayStrategyRouteEntity.java
@@ -0,0 +1,212 @@
+package com.nepxion.discovery.common.entity;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.nepxion.discovery.common.util.JsonUtil;
+
+public class GatewayStrategyRouteEntity implements Serializable {
+    private static final long serialVersionUID = 8552414941889295450L;
+
+    private String id;
+    private String uri;
+    private List<String> predicates = new ArrayList<String>();
+    private List<Predicate> userPredicates = new ArrayList<Predicate>();
+    private List<String> filters = new ArrayList<String>();
+    private List<Filter> userFilters = new ArrayList<Filter>();
+    private int order = 0;
+    private Map<String, Object> metadata = new HashMap<String, Object>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public List<String> getPredicates() {
+        return predicates;
+    }
+
+    public void setPredicates(List<String> predicates) {
+        this.predicates = predicates;
+    }
+
+    public List<Predicate> getUserPredicates() {
+        return userPredicates;
+    }
+
+    public void setUserPredicates(List<Predicate> userPredicates) {
+        this.userPredicates = userPredicates;
+    }
+
+    public List<String> getFilters() {
+        return filters;
+    }
+
+    public void setFilters(List<String> filters) {
+        this.filters = filters;
+    }
+
+    public List<Filter> getUserFilters() {
+        return userFilters;
+    }
+
+    public void setUserFilters(List<Filter> userFilters) {
+        this.userFilters = userFilters;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public Map<String, Object> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, Object> metadata) {
+        if (metadata != null) {
+            this.metadata = metadata;
+        }
+    }
+
+    public static class Predicate extends Clause {
+        private static final long serialVersionUID = -5113611207453059195L;
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            return EqualsBuilder.reflectionEquals(this, object);
+        }
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        }
+    }
+
+    public static class Filter extends Clause {
+        private static final long serialVersionUID = 4975847305584873755L;
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            return EqualsBuilder.reflectionEquals(this, object);
+        }
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        }
+    }
+
+    public static class Clause implements Serializable {
+        private static final long serialVersionUID = -6284675851623689077L;
+
+        private String name;
+        private Map<String, String> args = new LinkedHashMap<>();
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Map<String, String> getArgs() {
+            return args;
+        }
+
+        public void setArgs(Map<String, String> args) {
+            this.args = args;
+        }
+
+        public void addArg(String key, String value) {
+            this.args.put(key, value);
+        }
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            return EqualsBuilder.reflectionEquals(this, object);
+        }
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        }
+    }
+
+    public static <T extends Clause> String parseClauseJson(List<T> clauses) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Clause clause : clauses) {
+            stringBuilder.append(String.format("%s=%s, ", clause.getName(), JsonUtil.toJson(clause.getArgs())));
+        }
+
+        if (stringBuilder.length() > 0) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        }
+
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return EqualsBuilder.reflectionEquals(this, object);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
