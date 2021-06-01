1fb471153060b9de8d9fcb5c1ebca9b47ffec66d
HaojunRen <1394997@qq.com>
2021-06-01 20:44:08 +0800

    Add new format to spring_application_uuid

--- discovery-plugin-framework/discovery-plugin-framework-starter/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginEnvironmentPostProcessor.java
+++ discovery-plugin-framework/discovery-plugin-framework-starter/src/main/java/com/nepxion/discovery/plugin/framework/context/PluginEnvironmentPostProcessor.java
@@ -9,8 +9,6 @@ package com.nepxion.discovery.plugin.framework.context;
  * @version 1.0
  */
 
-import java.util.UUID;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
@@ -19,12 +17,13 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.util.UuidUtil;
 
 public class PluginEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         if (StringUtils.equals(environment.getClass().getName(), StandardEnvironment.class.getName())) {
-            String uuid = UUID.randomUUID().toString();
+            String uuid = UuidUtil.getTimeUUID();
             System.setProperty(DiscoveryConstant.SPRING_APPLICATION_UUID, uuid);
         }
     }
