2f259310bf791e03dd87d1d6523eb645bd7217f2
HaojunRen <1394997@qq.com>
2021-06-09 21:13:50 +0800

    Refactory GatewayType

--- discovery-plugin-strategy/discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyEnvironmentPostProcessor.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter-zuul/src/main/java/com/nepxion/discovery/plugin/strategy/zuul/context/ZuulStrategyEnvironmentPostProcessor.java
@@ -17,15 +17,15 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.StandardEnvironment;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.entity.GatewayType;
 import com.nepxion.discovery.common.entity.ServiceType;
-import com.nepxion.discovery.plugin.strategy.zuul.constant.ZuulStrategyConstant;
 
 public class ZuulStrategyEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         if (StringUtils.equals(environment.getClass().getName(), StandardEnvironment.class.getName())) {
             System.setProperty(DiscoveryConstant.SPRING_APPLICATION_TYPE, ServiceType.GATEWAY.toString());
-            System.setProperty(DiscoveryConstant.SPRING_APPLICATION_GATEWAY_TYPE, ZuulStrategyConstant.ZUUL);
+            System.setProperty(DiscoveryConstant.SPRING_APPLICATION_GATEWAY_TYPE, GatewayType.ZUUL.toString());
         }
     }
 
