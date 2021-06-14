57e402fd430d9add55d957ef3574b5573aa7fcec
HaojunRen <1394997@qq.com>
2021-06-14 21:09:45 +0800

    Add comments

--- discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/aop/WebClientStrategyInterceptor.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/aop/WebClientStrategyInterceptor.java
@@ -57,33 +57,57 @@ public class WebClientStrategyInterceptor extends AbstractStrategyInterceptor im
     public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
         ClientRequest.Builder requestBuilder = ClientRequest.from(request);
 
+        // 拦截打印输入的Header
         interceptInputHeader();
 
+        // 处理内部Header的转发
         applyInnerHeader(requestBuilder);
+
+        // 处理外部Header的转发
         applyOuterHeader(request, requestBuilder);
 
         ClientRequest newRequest = requestBuilder.build();
 
+        // 拦截打印输出的Header
         interceptOutputHeader(newRequest);
 
         return next.exchange(newRequest);
     }
 
+    // 处理内部Header的转发，即把本地服务的相关属性封装成Header转发到下游服务去
     private void applyInnerHeader(ClientRequest.Builder requestBuilder) {
+        // 设置本地组名到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_GROUP, pluginAdapter.getGroup());
+
+        // 设置本地服务类型到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_TYPE, pluginAdapter.getServiceType());
+
+        // 设置本地服务APPID到Header中，并全链路传递
         String serviceAppId = pluginAdapter.getServiceAppId();
         if (StringUtils.isNotEmpty(serviceAppId)) {
             requestBuilder.header(DiscoveryConstant.N_D_SERVICE_APP_ID, serviceAppId);
         }
+
+        // 设置本地服务名到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_ID, pluginAdapter.getServiceId());
+
+        // 设置本地服务IP地址和端口到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_ADDRESS, pluginAdapter.getHost() + ":" + pluginAdapter.getPort());
+
+        // 设置本地服务版本号到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_VERSION, pluginAdapter.getVersion());
+
+        // 设置本地服务区域值到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_REGION, pluginAdapter.getRegion());
+
+        // 设置本地服务环境值到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_ENVIRONMENT, pluginAdapter.getEnvironment());
+
+        // 设置本地服务可用区到Header中，并全链路传递
         requestBuilder.header(DiscoveryConstant.N_D_SERVICE_ZONE, pluginAdapter.getZone());
     }
 
+    // 处理外部Header的转发，即外部服务传递过来的Header，中继转发到下游服务去
     private void applyOuterHeader(ClientRequest request, ClientRequest.Builder requestBuilder) {
         Enumeration<String> headerNames = strategyContextHolder.getHeaderNames();
         if (headerNames != null) {
@@ -106,48 +130,64 @@ public class WebClientStrategyInterceptor extends AbstractStrategyInterceptor im
 
         if (webClientCoreHeaderTransmissionEnabled) {
             HttpHeaders headers = request.headers();
+
+            // 设置版本匹配路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_VERSION))) {
                 String routeVersion = strategyContextHolder.getRouteVersion();
                 if (StringUtils.isNotEmpty(routeVersion)) {
                     requestBuilder.header(DiscoveryConstant.N_D_VERSION, routeVersion);
                 }
             }
+
+            // 设置区域匹配路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_REGION))) {
                 String routeRegion = strategyContextHolder.getRouteRegion();
                 if (StringUtils.isNotEmpty(routeRegion)) {
                     requestBuilder.header(DiscoveryConstant.N_D_REGION, routeRegion);
                 }
             }
+
+            // 设置环境匹配路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_ENVIRONMENT))) {
                 String routeEnvironment = strategyContextHolder.getRouteEnvironment();
                 if (StringUtils.isNotEmpty(routeEnvironment)) {
                     requestBuilder.header(DiscoveryConstant.N_D_ENVIRONMENT, routeEnvironment);
                 }
             }
+
+            // 设置IP地址和端口匹配路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_ADDRESS))) {
                 String routeAddress = strategyContextHolder.getRouteAddress();
                 if (StringUtils.isNotEmpty(routeAddress)) {
                     requestBuilder.header(DiscoveryConstant.N_D_ADDRESS, routeAddress);
                 }
             }
+
+            // 设置版本权重路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_VERSION_WEIGHT))) {
                 String routeVersionWeight = strategyContextHolder.getRouteVersionWeight();
                 if (StringUtils.isNotEmpty(routeVersionWeight)) {
                     requestBuilder.header(DiscoveryConstant.N_D_VERSION_WEIGHT, routeVersionWeight);
                 }
             }
+
+            // 设置区域权重路由的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_REGION_WEIGHT))) {
                 String routeRegionWeight = strategyContextHolder.getRouteRegionWeight();
                 if (StringUtils.isNotEmpty(routeRegionWeight)) {
                     requestBuilder.header(DiscoveryConstant.N_D_REGION_WEIGHT, routeRegionWeight);
                 }
             }
+
+            // 设置全局唯一ID黑名单屏蔽的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_ID_BLACKLIST))) {
                 String routeIdBlacklist = strategyContextHolder.getRouteIdBlacklist();
                 if (StringUtils.isNotEmpty(routeIdBlacklist)) {
                     requestBuilder.header(DiscoveryConstant.N_D_ID_BLACKLIST, routeIdBlacklist);
                 }
             }
+
+            // 设置IP地址和端口黑名单屏蔽的配置到Header中
             if (CollectionUtils.isEmpty(headers.get(DiscoveryConstant.N_D_ADDRESS_BLACKLIST))) {
                 String routeAddressBlacklist = strategyContextHolder.getRouteAddressBlacklist();
                 if (StringUtils.isNotEmpty(routeAddressBlacklist)) {
