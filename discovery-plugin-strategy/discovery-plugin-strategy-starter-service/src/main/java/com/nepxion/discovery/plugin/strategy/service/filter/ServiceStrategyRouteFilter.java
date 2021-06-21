04f1b099c24f0dd0e3faf024a85121755675f3fe
HaojunRen <1394997@qq.com>
2021-06-21 14:00:46 +0800

    Compatible for ServiceStrategyRouteFilter

--- discovery-plugin-strategy/discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/filter/ServiceStrategyRouteFilter.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter-service/src/main/java/com/nepxion/discovery/plugin/strategy/service/filter/ServiceStrategyRouteFilter.java
@@ -9,8 +9,10 @@ package com.nepxion.discovery.plugin.strategy.service.filter;
  * @version 1.0
  */
 
+import org.springframework.core.Ordered;
+
 import com.nepxion.discovery.plugin.strategy.filter.StrategyRouteFilter;
 
-public abstract class ServiceStrategyRouteFilter extends ServiceStrategyFilter implements StrategyRouteFilter {
+public abstract class ServiceStrategyRouteFilter extends ServiceStrategyFilter implements StrategyRouteFilter, Ordered {
 
 }
