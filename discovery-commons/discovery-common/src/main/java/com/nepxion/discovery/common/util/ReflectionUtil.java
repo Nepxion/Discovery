8fb81c49fb8bd72f7800f1e10f6c234df985f083
HaojunRen <1394997@qq.com>
2021-06-22 17:06:52 +0800

    Refactory ReflectionUtil

--- discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/util/ReflectionUtil.java
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/util/ReflectionUtil.java
@@ -10,24 +10,39 @@ package com.nepxion.discovery.common.util;
  */
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ReflectionUtil {
+    private static Map<String, Method> methodCache = new ConcurrentHashMap<String, Method>();
     private static Map<String, Field> fieldCache = new ConcurrentHashMap<String, Field>();
+
+    public static Object invoke(Class<?> clazz, Object instance, String name, Class<?>[] parameterTypes, Object[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        String key = clazz.getName() + name;
+
+        Method method = methodCache.get(key);
+        if (method == null) {
+            method = clazz.getMethod(name, parameterTypes);
+
+            methodCache.put(key, method);
+        }
+
+        return method.invoke(instance, args);
+    }
 
     public static Object getValue(Class<?> clazz, Object instance, String name) throws NoSuchFieldException, IllegalAccessException {
         String key = clazz.getName() + name;
 
-        Field cacheField = fieldCache.get(key);
-        if (cacheField == null) {
-            Field field = clazz.getDeclaredField(name);
+        Field field = fieldCache.get(key);
+        if (field == null) {
+            field = clazz.getDeclaredField(name);
             field.setAccessible(true);
 
             fieldCache.put(key, field);
-            cacheField = field;
         }
 
-        return cacheField.get(instance);
+        return field.get(instance);
     }
 }
