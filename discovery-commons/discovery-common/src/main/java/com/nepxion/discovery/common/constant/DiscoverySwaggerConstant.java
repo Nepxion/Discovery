7cf99989aa152041e7c02e42dfbbfbc35e8fb0ca
HaojunRen <1394997@qq.com>
2021-06-26 21:44:08 +0800

    Refactory Swagger module

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoverySwaggerConstant.java
@@ -0,0 +1,31 @@
+package com.nepxion.discovery.common.constant;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public class DiscoverySwaggerConstant {
+    public static final String SWAGGER_SERVICE_ENABLED = "swagger.service.enabled";
+    public static final String SWAGGER_SERVICE_BASE_GROUP = "swagger.service.base.group";
+    public static final String SWAGGER_SERVICE_SCAN_GROUP = "swagger.service.scan.group";
+    public static final String SWAGGER_SERVICE_SCAN_PACKAGES = "swagger.service.scan.packages";
+    public static final String SWAGGER_SERVICE_DESCRIPTION = "swagger.service.description";
+    public static final String SWAGGER_SERVICE_VERSION = "swagger.service.version";
+    public static final String SWAGGER_SERVICE_LICENSE_NAME = "swagger.service.license.name";
+    public static final String SWAGGER_SERVICE_LICENSE_URL = "swagger.service.license.url";
+    public static final String SWAGGER_SERVICE_CONTACT_NAME = "swagger.service.contact.name";
+    public static final String SWAGGER_SERVICE_CONTACT_URL = "swagger.service.contact.url";
+    public static final String SWAGGER_SERVICE_CONTACT_EMAIL = "swagger.service.contact.email";
+    public static final String SWAGGER_SERVICE_TERMSOFSERVICE_URL = "swagger.service.termsOfService.url";
+
+    public static final String SWAGGER_SERVICE_LICENSE_NAME_VALUE = "Apache License 2.0";
+    public static final String SWAGGER_SERVICE_LICENSE_URL_VALUE = "http://www.apache.org/licenses/LICENSE-2.0";
+    public static final String SWAGGER_SERVICE_CONTACT_URL_VALUE = "https://github.com/Nepxion/Discovery";
+    public static final String SWAGGER_SERVICE_CONTACT_EMAIL_VALUE = "1394997@qq.com";
+    public static final String SWAGGER_SERVICE_TERMSOFSERVICE_URL_VALUE = "http://www.nepxion.com";
+}
