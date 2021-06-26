52e39f752780c57793933c9cfccb096ee7d9ed18
HaojunRen <1394997@qq.com>
2021-06-26 21:48:25 +0800

    Refactory Swagger module

--- discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/configuration/SwaggerConfiguration.java
+++ discovery-plugin-admin-center/discovery-plugin-admin-center-starter/src/main/java/com/nepxion/discovery/plugin/admincenter/configuration/SwaggerConfiguration.java
@@ -33,51 +33,75 @@ import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.common.constant.DiscoverySwaggerConstant;
 
 @Configuration
 @EnableSwagger2
 @ConditionalOnClass(HandlerMapping.class)
-@ConditionalOnProperty(value = "swagger.service.enabled", matchIfMissing = true)
+@ConditionalOnProperty(value = DiscoverySwaggerConstant.SWAGGER_SERVICE_ENABLED, matchIfMissing = true)
 public class SwaggerConfiguration {
     public static final String BASE_PACKAGE = "com.nepxion.discovery.plugin.admincenter.endpoint";
+    public static final String DESCRIPTION = "Admin Center Restful APIs";
 
-    @Value("${spring.application.name}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_BASE_GROUP + ":" + DiscoveryConstant.NEPXION_DISCOVERY + "}")
+    private String baseGroup;
+
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_SCAN_GROUP + ":}")
+    private String scanGroup;
+
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_SCAN_PACKAGES + ":}")
+    private String scanPackages;
+
+    @Value("${" + DiscoveryConstant.SPRING_APPLICATION_NAME + "}")
     private String serviceName;
 
-    @Value("${swagger.service.description:Admin Center Restful APIs}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_DESCRIPTION + ":" + DESCRIPTION + "}")
     private String description;
 
-    @Value("${swagger.service.version:1.0.0}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_VERSION + ":" + DiscoveryConstant.DISCOVERY_VERSION + "}")
     private String version;
 
-    @Value("${swagger.service.license.name:Apache License 2.0}")
-    private String license;
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_LICENSE_NAME + ":" + DiscoverySwaggerConstant.SWAGGER_SERVICE_LICENSE_NAME_VALUE + "}")
+    private String licenseName;
 
-    @Value("${swagger.service.license.url:http://www.apache.org/licenses/LICENSE-2.0}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_LICENSE_URL + ":" + DiscoverySwaggerConstant.SWAGGER_SERVICE_LICENSE_URL_VALUE + "}")
     private String licenseUrl;
 
-    @Value("${swagger.service.contact.name:Haojun Ren}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_CONTACT_NAME + ":" + DiscoveryConstant.NEPXION_FIRST_UPPERCASE + "}")
     private String contactName;
 
-    @Value("${swagger.service.contact.url:https://github.com/Nepxion/Discovery}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_CONTACT_URL + ":" + DiscoverySwaggerConstant.SWAGGER_SERVICE_CONTACT_URL_VALUE + "}")
     private String contactUrl;
 
-    @Value("${swagger.service.contact.email:1394997@qq.com}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_CONTACT_EMAIL + ":" + DiscoverySwaggerConstant.SWAGGER_SERVICE_CONTACT_EMAIL_VALUE + "}")
     private String contactEmail;
 
-    @Value("${swagger.service.termsOfServiceUrl:http://www.nepxion.com}")
+    @Value("${" + DiscoverySwaggerConstant.SWAGGER_SERVICE_TERMSOFSERVICE_URL + ":" + DiscoverySwaggerConstant.SWAGGER_SERVICE_TERMSOFSERVICE_URL_VALUE + "}")
     private String termsOfServiceUrl;
 
     @Autowired(required = false)
     private List<Parameter> swaggerHeaderParameters;
 
     @Bean("discoveryDocket")
-    public Docket createRestApi() {
+    public Docket discoveryDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("discovery")
+                .groupName(baseGroup)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(SwaggerConfiguration.basePackage(BASE_PACKAGE)) // 扫描该包下的所有需要在Swagger中展示的API，@ApiIgnore注解标注的除外
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(swaggerHeaderParameters);
+    }
+
+    @Bean("scanDocket")
+    @ConditionalOnProperty(name = DiscoverySwaggerConstant.SWAGGER_SERVICE_SCAN_GROUP)
+    public Docket scanDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(scanGroup)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(SwaggerConfiguration.basePackage(scanPackages)) // 扫描该包下的所有需要在Swagger中展示的API，@ApiIgnore注解标注的除外
                 .paths(PathSelectors.any())
                 .build()
                 .globalOperationParameters(swaggerHeaderParameters);
@@ -88,7 +112,7 @@ public class SwaggerConfiguration {
                 .title(serviceName)
                 .description(description)
                 .version(version)
-                .license(license)
+                .license(licenseName)
                 .licenseUrl(licenseUrl)
                 .contact(new Contact(contactName, contactUrl, contactEmail))
                 .termsOfServiceUrl(termsOfServiceUrl)
