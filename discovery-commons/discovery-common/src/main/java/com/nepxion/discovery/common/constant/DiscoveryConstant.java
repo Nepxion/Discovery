14b3b3c5646bd34e6a1f5e36c30f54f5faf50973
HaojunRen <1394997@qq.com>
2021-06-29 17:08:08 +0800

    Add BEARER to DiscoveryConstant

--- discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/constant/DiscoveryConstant.java
@@ -111,6 +111,7 @@ public class DiscoveryConstant {
     public static final String N_D_ADDRESS_BLACKLIST = "n-d-address-blacklist";
 
     public static final String N_D_ACCESS_TOKEN = "n-d-access-token";
+    public static final String BEARER = "Bearer";
 
     public static final String BLUE_GREEN = "blue-green";
     public static final String BLUE_GREEN_BASIC = "blue-green-basic";
