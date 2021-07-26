9d862e9c01278ede821492dddd0be3f065c2eb06
HaojunRen <1394997@qq.com>
2021-07-26 14:08:41 +0800

    Refactory WeightRandomLoadBalance

---
+++ discovery-plugin-framework/discovery-plugin-framework-starter/src/main/java/com/nepxion/discovery/plugin/framework/loadbalance/weight/RuleWeightRandomLoadBalance.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.plugin.framework.loadbalance.weight;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import com.nepxion.discovery.plugin.framework.loadbalance.WeightRandomLoadBalance;
+
+public interface RuleWeightRandomLoadBalance<T> extends WeightRandomLoadBalance<T> {
+
+}
