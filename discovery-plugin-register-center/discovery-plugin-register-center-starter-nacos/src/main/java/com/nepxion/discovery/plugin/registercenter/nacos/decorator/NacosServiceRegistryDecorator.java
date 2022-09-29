fea98e3b8ae61dff002faa70a5ec777962506cc8
HaojunRen <1394997@qq.com>
2022-09-29 10:39:26 +0800

    Remove unnecessary configs and features

--- discovery-plugin-register-center/discovery-plugin-register-center-starter-nacos/src/main/java/com/nepxion/discovery/plugin/registercenter/nacos/decorator/NacosServiceRegistryDecorator.java
+++ discovery-plugin-register-center/discovery-plugin-register-center-starter-nacos/src/main/java/com/nepxion/discovery/plugin/registercenter/nacos/decorator/NacosServiceRegistryDecorator.java
@@ -12,12 +12,10 @@ package com.nepxion.discovery.plugin.registercenter.nacos.decorator;
 import org.springframework.beans.BeansException;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.alibaba.cloud.nacos.NacosServiceManager;
 import com.alibaba.cloud.nacos.registry.NacosServiceRegistry;
-import com.nepxion.discovery.plugin.framework.context.PluginContextAware;
 import com.nepxion.discovery.plugin.framework.listener.register.RegisterListenerExecutor;
 
 public class NacosServiceRegistryDecorator extends NacosServiceRegistry {
@@ -25,26 +23,21 @@ public class NacosServiceRegistryDecorator extends NacosServiceRegistry {
 
     private NacosServiceRegistry serviceRegistry;
     private ConfigurableApplicationContext applicationContext;
-    private ConfigurableEnvironment environment;
 
     public NacosServiceRegistryDecorator(NacosServiceManager nacosServiceManager, NacosDiscoveryProperties nacosDiscoveryProperties, NacosServiceRegistry serviceRegistry, ConfigurableApplicationContext applicationContext) {
         super(nacosServiceManager, nacosDiscoveryProperties);
 
         this.serviceRegistry = serviceRegistry;
         this.applicationContext = applicationContext;
-        this.environment = applicationContext.getEnvironment();
     }
 
     @Override
     public void register(Registration registration) {
-        Boolean registerControlEnabled = PluginContextAware.isRegisterControlEnabled(environment);
-        if (registerControlEnabled) {
-            try {
-                RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
-                registerListenerExecutor.onRegister(registration);
-            } catch (BeansException e) {
-                // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
-            }
+        try {
+            RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
+            registerListenerExecutor.onRegister(registration);
+        } catch (BeansException e) {
+            // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
         }
 
         serviceRegistry.register(registration);
@@ -52,14 +45,11 @@ public class NacosServiceRegistryDecorator extends NacosServiceRegistry {
 
     @Override
     public void deregister(Registration registration) {
-        Boolean registerControlEnabled = PluginContextAware.isRegisterControlEnabled(environment);
-        if (registerControlEnabled) {
-            try {
-                RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
-                registerListenerExecutor.onDeregister(registration);
-            } catch (BeansException e) {
-                // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
-            }
+        try {
+            RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
+            registerListenerExecutor.onDeregister(registration);
+        } catch (BeansException e) {
+            // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
         }
 
         serviceRegistry.deregister(registration);
@@ -67,14 +57,11 @@ public class NacosServiceRegistryDecorator extends NacosServiceRegistry {
 
     @Override
     public void setStatus(Registration registration, String status) {
-        Boolean registerControlEnabled = PluginContextAware.isRegisterControlEnabled(environment);
-        if (registerControlEnabled) {
-            try {
-                RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
-                registerListenerExecutor.onSetStatus(registration, status);
-            } catch (BeansException e) {
-                // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
-            }
+        try {
+            RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
+            registerListenerExecutor.onSetStatus(registration, status);
+        } catch (BeansException e) {
+            // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
         }
 
         serviceRegistry.setStatus(registration, status);
@@ -87,20 +74,13 @@ public class NacosServiceRegistryDecorator extends NacosServiceRegistry {
 
     @Override
     public void close() {
-        Boolean registerControlEnabled = PluginContextAware.isRegisterControlEnabled(environment);
-        if (registerControlEnabled) {
-            try {
-                RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
-                registerListenerExecutor.onClose();
-            } catch (BeansException e) {
-                // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
-            }
+        try {
+            RegisterListenerExecutor registerListenerExecutor = applicationContext.getBean(RegisterListenerExecutor.class);
+            registerListenerExecutor.onClose();
+        } catch (BeansException e) {
+            // LOG.warn("Get bean for RegisterListenerExecutor failed, ignore to executor listener");
         }
 
         serviceRegistry.close();
-    }
-
-    public ConfigurableEnvironment getEnvironment() {
-        return environment;
     }
 }
