1ae6cf635c07efb2a078cb0a36bbd73b65e44b06
HaojunRen <1394997@qq.com>
2022-10-14 21:08:13 +0800

    Support automation release open api in Console StrategyEndpoint

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/entity/ConditionRouteStrategy.java
@@ -0,0 +1,56 @@
+package com.nepxion.discovery.common.entity;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class ConditionRouteStrategy implements Serializable {
+    private static final long serialVersionUID = -6036447421562945200L;
+
+    private List<String> service;
+    private boolean condition;
+
+    public List<String> getService() {
+        return service;
+    }
+
+    public void setService(List<String> service) {
+        this.service = service;
+    }
+
+    public boolean isCondition() {
+        return condition;
+    }
+
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return EqualsBuilder.reflectionEquals(this, object);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
