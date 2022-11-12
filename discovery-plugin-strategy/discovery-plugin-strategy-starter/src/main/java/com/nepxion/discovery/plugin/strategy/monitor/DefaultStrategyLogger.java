2405886078aa65bae1980712a64da997ddfc87c6
HaojunRen <1394997@qq.com>
2022-11-12 14:26:13 +0800

    Add StrategyTracerHeadersInjector to support customizing headers tracing, logger and alarm

--- discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/monitor/DefaultStrategyLogger.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/monitor/DefaultStrategyLogger.java
@@ -9,8 +9,10 @@ package com.nepxion.discovery.plugin.strategy.monitor;
  * @version 1.0
  */
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
@@ -188,6 +190,16 @@ public class DefaultStrategyLogger implements StrategyLogger {
         String routeAddressBlacklist = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ADDRESS_BLACKLIST);
         if (StringUtils.isNotEmpty(routeAddressBlacklist)) {
             System.out.println(DiscoveryConstant.N_D_ADDRESS_BLACKLIST + "=" + routeAddressBlacklist);
+        }
+
+        List<String> tracerHeaderNameList = strategyMonitorContext.getTracerHeaderNameList();
+        if (CollectionUtils.isNotEmpty(tracerHeaderNameList)) {
+            for (String tracerHeaderName : tracerHeaderNameList) {
+                String tracerHeaderValue = strategyContextHolder.getHeader(tracerHeaderName);
+                if (StringUtils.isNotEmpty(tracerHeaderValue)) {
+                    System.out.println(tracerHeaderName + "=" + tracerHeaderValue);
+                }
+            }
         }
 
         Map<String, String> customizationMap = strategyMonitorContext.getCustomizationMap();
