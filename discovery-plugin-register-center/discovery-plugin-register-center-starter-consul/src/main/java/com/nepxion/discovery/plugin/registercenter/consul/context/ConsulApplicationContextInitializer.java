04ed63b2011f282f6013a7130ad864dc22cb371a
HaojunRen <1394997@qq.com>
2022-11-13 09:51:11 +0800

    Add PluginMetaDataPreInstallation to support preinstalling extension metadatas

--- discovery-plugin-register-center/discovery-plugin-register-center-starter-consul/src/main/java/com/nepxion/discovery/plugin/registercenter/consul/context/ConsulApplicationContextInitializer.java
+++ discovery-plugin-register-center/discovery-plugin-register-center-starter-consul/src/main/java/com/nepxion/discovery/plugin/registercenter/consul/context/ConsulApplicationContextInitializer.java
@@ -10,6 +10,7 @@ package com.nepxion.discovery.plugin.registercenter.consul.context;
  */
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
@@ -20,12 +21,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
-import com.nepxion.discovery.common.constant.DiscoveryExtensionConstant;
 import com.nepxion.discovery.common.constant.DiscoveryMetaDataConstant;
 import com.nepxion.discovery.common.entity.DiscoveryType;
 import com.nepxion.discovery.plugin.framework.adapter.ApplicationInfoAdapter;
 import com.nepxion.discovery.plugin.framework.context.PluginApplicationContextInitializer;
 import com.nepxion.discovery.plugin.framework.context.PluginContextAware;
+import com.nepxion.discovery.plugin.framework.context.PluginMetaDataPreInstallation;
 import com.nepxion.discovery.plugin.framework.util.MetadataUtil;
 import com.nepxion.discovery.plugin.registercenter.consul.decorator.ConsulServiceRegistryDecorator;
 
@@ -83,6 +84,10 @@ public class ConsulApplicationContextInitializer extends PluginApplicationContex
             metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_UUID + "=" + PluginContextAware.getApplicationUUId(environment));
             metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_PLUGIN + "=" + DiscoveryType.CONSUL);
             metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_VERSION + "=" + DiscoveryConstant.DISCOVERY_VERSION);
+            String agentVersion = System.getProperty(DiscoveryConstant.SPRING_APPLICATION_DISCOVERY_AGENT_VERSION);
+            if (StringUtils.isNotEmpty(agentVersion)) {
+                metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_AGENT_VERSION + "=" + agentVersion);
+            }
             metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_GROUP_KEY + "=" + groupKey);
             metadata.add(DiscoveryMetaDataConstant.SPRING_APPLICATION_CONTEXT_PATH + "=" + PluginContextAware.getContextPath(environment));
 
@@ -95,11 +100,12 @@ public class ConsulApplicationContextInitializer extends PluginApplicationContex
 
             }
 
-            for (int i = 0; i < DiscoveryExtensionConstant.EXTENSION_VERSION_KEYS.length; i++) {
-                String[] extensionVersionKeys = DiscoveryExtensionConstant.EXTENSION_VERSION_KEYS[i];
-                String extensionVersion = System.getProperty(extensionVersionKeys[0]);
-                if (StringUtils.isNotEmpty(extensionVersion)) {
-                    metadata.add(extensionVersionKeys[1] + "=" + extensionVersion);
+            for (Map.Entry<String, String> entry : PluginMetaDataPreInstallation.getMetadata().entrySet()) {
+                String versionKey = entry.getKey();
+                String metadataVersionKey = entry.getValue();
+                String versionValue = System.getProperty(versionKey);
+                if (StringUtils.isNotEmpty(versionValue)) {
+                    metadata.add(metadataVersionKey + "=" + versionValue);
                 }
             }
 
--- discovery-plugin-register-center/discovery-plugin-register-center-starter-nacos/src/main/java/com/nepxion/discovery/plugin/registercenter/nacos/context/NacosApplicationContextInitializer.java
+++ discovery-plugin-register-center/discovery-plugin-register-center-starter-nacos/src/main/java/com/nepxion/discovery/plugin/registercenter/nacos/context/NacosApplicationContextInitializer.java
@@ -21,12 +21,12 @@ import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
 import com.alibaba.cloud.nacos.NacosServiceManager;
 import com.alibaba.cloud.nacos.registry.NacosServiceRegistry;
 import com.nepxion.discovery.common.constant.DiscoveryConstant;
-import com.nepxion.discovery.common.constant.DiscoveryExtensionConstant;
 import com.nepxion.discovery.common.constant.DiscoveryMetaDataConstant;
 import com.nepxion.discovery.common.entity.DiscoveryType;
 import com.nepxion.discovery.plugin.framework.adapter.ApplicationInfoAdapter;
 import com.nepxion.discovery.plugin.framework.context.PluginApplicationContextInitializer;
 import com.nepxion.discovery.plugin.framework.context.PluginContextAware;
+import com.nepxion.discovery.plugin.framework.context.PluginMetaDataPreInstallation;
 import com.nepxion.discovery.plugin.framework.util.MetadataUtil;
 import com.nepxion.discovery.plugin.registercenter.nacos.decorator.NacosServiceRegistryDecorator;
 
@@ -86,6 +86,10 @@ public class NacosApplicationContextInitializer extends PluginApplicationContext
             metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_UUID, PluginContextAware.getApplicationUUId(environment));
             metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_PLUGIN, DiscoveryType.NACOS.toString());
             metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_VERSION, DiscoveryConstant.DISCOVERY_VERSION);
+            String agentVersion = System.getProperty(DiscoveryConstant.SPRING_APPLICATION_DISCOVERY_AGENT_VERSION);
+            if (StringUtils.isNotEmpty(agentVersion)) {
+                metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_DISCOVERY_AGENT_VERSION, agentVersion);
+            }
             metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_GROUP_KEY, groupKey);
             metadata.put(DiscoveryMetaDataConstant.SPRING_APPLICATION_CONTEXT_PATH, PluginContextAware.getContextPath(environment));
 
@@ -98,11 +102,12 @@ public class NacosApplicationContextInitializer extends PluginApplicationContext
 
             }
 
-            for (int i = 0; i < DiscoveryExtensionConstant.EXTENSION_VERSION_KEYS.length; i++) {
-                String[] extensionVersionKeys = DiscoveryExtensionConstant.EXTENSION_VERSION_KEYS[i];
-                String extensionVersion = System.getProperty(extensionVersionKeys[0]);
-                if (StringUtils.isNotEmpty(extensionVersion)) {
-                    metadata.put(extensionVersionKeys[1], extensionVersion);
+            for (Map.Entry<String, String> entry : PluginMetaDataPreInstallation.getMetadata().entrySet()) {
+                String versionKey = entry.getKey();
+                String metadataVersionKey = entry.getValue();
+                String versionValue = System.getProperty(versionKey);
+                if (StringUtils.isNotEmpty(versionValue)) {
+                    metadata.put(metadataVersionKey, versionValue);
                 }
             }
 
