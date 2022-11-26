348376e33d1f2e86a93abf3d880853039b99c91f
HaojunRen <1394997@qq.com>
2022-11-26 16:55:40 +0800

    Add LogbackLevelColor

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/logback/LogbackLevelColor.java
@@ -0,0 +1,40 @@
+package com.nepxion.discovery.common.logback;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ANSIConstants;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+// Refer to color definition with https://logback.qos.ch/manual/layouts.html#coloring
+public class LogbackLevelColor extends ForegroundCompositeConverterBase<ILoggingEvent> {
+    @Override
+    protected String getForegroundColorCode(ILoggingEvent event) {
+        Level level = event.getLevel();
+        switch (level.toInt()) {
+            // ERROR等级为红色
+            case Level.ERROR_INT:
+                return ANSIConstants.RED_FG;
+            // WARN等级为黄色
+            case Level.WARN_INT:
+                return ANSIConstants.YELLOW_FG;
+            // INFO等级为蓝色
+            case Level.INFO_INT:
+                return ANSIConstants.GREEN_FG;
+            // DEBUG等级为绿色
+            case Level.DEBUG_INT:
+                return ANSIConstants.BLUE_FG;
+            // 其他为默认颜色
+            default:
+                return ANSIConstants.DEFAULT_FG;
+        }
+    }
+}
