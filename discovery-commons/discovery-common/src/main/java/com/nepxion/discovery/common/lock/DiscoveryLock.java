c46e06fe531967d37977e14890f39a17cfecd147
HaojunRen <1394997@qq.com>
2022-11-27 22:14:22 +0800

    Add lock

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/lock/DiscoveryLock.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.common.lock;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+public interface DiscoveryLock {
+    boolean tryLock(String key);
+
+    void unlock(String key);
+}
