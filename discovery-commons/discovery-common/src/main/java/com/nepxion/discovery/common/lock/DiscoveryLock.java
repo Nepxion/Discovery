ffaa69af15bcbe8d1b3fb7bfc0a8ef90583ba19e
HaojunRen <1394997@qq.com>
2022-11-28 13:49:45 +0800

    Add Lock

---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/lock/DiscoveryLock.java
@@ -0,0 +1,26 @@
+package com.nepxion.discovery.common.lock;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import java.util.List;
+
+public interface DiscoveryLock {
+    // 尝试获取锁
+    boolean tryLock(String key);
+
+    // 释放锁
+    void unlock(String key);
+
+    // 获取被持有的所有锁名称列表
+    List<String> getHeldLocks(DiscoveryLockHeldType lockHeldType);
+
+    // 进程结束时销毁锁
+    void destroy() throws Exception;
+}
