dd6adcba33d43c9f21a308cf8bcad90e142f7fea
HaojunRen <1394997@qq.com>
2022-12-05 17:08:33 +0800

    System.out.println changes to log info

--- discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/monitor/DefaultStrategyLogger.java
+++ discovery-plugin-strategy/discovery-plugin-strategy-starter/src/main/java/com/nepxion/discovery/plugin/strategy/monitor/DefaultStrategyLogger.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,6 +26,8 @@ import com.nepxion.discovery.plugin.strategy.context.StrategyContextHolder;
 import com.nepxion.discovery.plugin.strategy.context.StrategyHeaderContext;
 
 public class DefaultStrategyLogger implements StrategyLogger {
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultStrategyLogger.class);
+
     @Autowired
     protected PluginAdapter pluginAdapter;
 
@@ -32,7 +36,7 @@ public class DefaultStrategyLogger implements StrategyLogger {
 
     @Autowired
     protected StrategyMonitorContext strategyMonitorContext;
-    
+
     @Autowired
     protected StrategyHeaderContext strategyHeaderContext;
 
@@ -100,99 +104,101 @@ public class DefaultStrategyLogger implements StrategyLogger {
             return;
         }
 
-        System.out.println("----------------------- Logger Debug -----------------------");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+        stringBuilder.append("--------------- Strategy Logger Information ----------------").append("\n");
         String traceId = strategyMonitorContext.getTraceId();
         String spanId = strategyMonitorContext.getSpanId();
         if (StringUtils.isNotEmpty(traceId)) {
-            System.out.println(DiscoveryConstant.TRACE_ID + "=" + traceId);
+            stringBuilder.append(DiscoveryConstant.TRACE_ID + "=" + traceId).append("\n");
         }
         if (StringUtils.isNotEmpty(spanId)) {
-            System.out.println(DiscoveryConstant.SPAN_ID + "=" + spanId);
+            stringBuilder.append(DiscoveryConstant.SPAN_ID + "=" + spanId).append("\n");
         }
-        System.out.println(DiscoveryConstant.N_D_SERVICE_GROUP + "=" + pluginAdapter.getGroup());
-        System.out.println(DiscoveryConstant.N_D_SERVICE_TYPE + "=" + pluginAdapter.getServiceType());
+        stringBuilder.append(DiscoveryConstant.N_D_SERVICE_GROUP + "=" + pluginAdapter.getGroup()).append("\n");
+        stringBuilder.append(DiscoveryConstant.N_D_SERVICE_TYPE + "=" + pluginAdapter.getServiceType()).append("\n");
         String serviceAppId = pluginAdapter.getServiceAppId();
         if (StringUtils.isNotEmpty(serviceAppId)) {
-            System.out.println(DiscoveryConstant.N_D_SERVICE_APP_ID + "=" + serviceAppId);
+            stringBuilder.append(DiscoveryConstant.N_D_SERVICE_APP_ID + "=" + serviceAppId).append("\n");
         }
-        System.out.println(DiscoveryConstant.N_D_SERVICE_ID + "=" + pluginAdapter.getServiceId());
-        System.out.println(DiscoveryConstant.N_D_SERVICE_ADDRESS + "=" + pluginAdapter.getHost() + ":" + pluginAdapter.getPort());
+        stringBuilder.append(DiscoveryConstant.N_D_SERVICE_ID + "=" + pluginAdapter.getServiceId()).append("\n");
+        stringBuilder.append(DiscoveryConstant.N_D_SERVICE_ADDRESS + "=" + pluginAdapter.getHost() + ":" + pluginAdapter.getPort()).append("\n");
         String version = pluginAdapter.getVersion();
         if (StringUtils.isNotEmpty(version) && !StringUtils.equals(version, DiscoveryConstant.DEFAULT)) {
-            System.out.println(DiscoveryConstant.N_D_SERVICE_VERSION + "=" + version);
+            stringBuilder.append(DiscoveryConstant.N_D_SERVICE_VERSION + "=" + version).append("\n");
         }
         String region = pluginAdapter.getRegion();
         if (StringUtils.isNotEmpty(region) && !StringUtils.equals(region, DiscoveryConstant.DEFAULT)) {
-            System.out.println(DiscoveryConstant.N_D_SERVICE_REGION + "=" + region);
+            stringBuilder.append(DiscoveryConstant.N_D_SERVICE_REGION + "=" + region).append("\n");
         }
         String environment = pluginAdapter.getEnvironment();
         if (StringUtils.isNotEmpty(environment) && !StringUtils.equals(environment, DiscoveryConstant.DEFAULT)) {
-            System.out.println(DiscoveryConstant.N_D_SERVICE_ENVIRONMENT + "=" + environment);
+            stringBuilder.append(DiscoveryConstant.N_D_SERVICE_ENVIRONMENT + "=" + environment).append("\n");
         }
         String zone = pluginAdapter.getZone();
         if (StringUtils.isNotEmpty(zone) && !StringUtils.equals(zone, DiscoveryConstant.DEFAULT)) {
-            System.out.println(DiscoveryConstant.N_D_SERVICE_ZONE + "=" + zone);
+            stringBuilder.append(DiscoveryConstant.N_D_SERVICE_ZONE + "=" + zone).append("\n");
         }
 
         String routeVersion = strategyContextHolder.getHeader(DiscoveryConstant.N_D_VERSION);
         if (StringUtils.isNotEmpty(routeVersion)) {
-            System.out.println(DiscoveryConstant.N_D_VERSION + "=" + routeVersion);
+            stringBuilder.append(DiscoveryConstant.N_D_VERSION + "=" + routeVersion).append("\n");
         }
         String routeRegion = strategyContextHolder.getHeader(DiscoveryConstant.N_D_REGION);
         if (StringUtils.isNotEmpty(routeRegion)) {
-            System.out.println(DiscoveryConstant.N_D_REGION + "=" + routeRegion);
+            stringBuilder.append(DiscoveryConstant.N_D_REGION + "=" + routeRegion).append("\n");
         }
         String routeEnvironment = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ENVIRONMENT);
         if (StringUtils.isNotEmpty(routeEnvironment)) {
-            System.out.println(DiscoveryConstant.N_D_ENVIRONMENT + "=" + routeEnvironment);
+            stringBuilder.append(DiscoveryConstant.N_D_ENVIRONMENT + "=" + routeEnvironment).append("\n");
         }
         String routeAddress = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ADDRESS);
         if (StringUtils.isNotEmpty(routeAddress)) {
-            System.out.println(DiscoveryConstant.N_D_ADDRESS + "=" + routeAddress);
+            stringBuilder.append(DiscoveryConstant.N_D_ADDRESS + "=" + routeAddress).append("\n");
         }
         String routeVersionWeight = strategyContextHolder.getHeader(DiscoveryConstant.N_D_VERSION_WEIGHT);
         if (StringUtils.isNotEmpty(routeVersionWeight)) {
-            System.out.println(DiscoveryConstant.N_D_VERSION_WEIGHT + "=" + routeVersionWeight);
+            stringBuilder.append(DiscoveryConstant.N_D_VERSION_WEIGHT + "=" + routeVersionWeight).append("\n");
         }
         String routeRegionWeight = strategyContextHolder.getHeader(DiscoveryConstant.N_D_REGION_WEIGHT);
         if (StringUtils.isNotEmpty(routeRegionWeight)) {
-            System.out.println(DiscoveryConstant.N_D_REGION_WEIGHT + "=" + routeRegionWeight);
+            stringBuilder.append(DiscoveryConstant.N_D_REGION_WEIGHT + "=" + routeRegionWeight).append("\n");
         }
         String routeVersionPrefer = strategyContextHolder.getHeader(DiscoveryConstant.N_D_VERSION_PREFER);
         if (StringUtils.isNotEmpty(routeVersionPrefer)) {
-            System.out.println(DiscoveryConstant.N_D_VERSION_PREFER + "=" + routeVersionPrefer);
+            stringBuilder.append(DiscoveryConstant.N_D_VERSION_PREFER + "=" + routeVersionPrefer).append("\n");
         }
         String routeVersionFailover = strategyContextHolder.getHeader(DiscoveryConstant.N_D_VERSION_FAILOVER);
         if (StringUtils.isNotEmpty(routeVersionFailover)) {
-            System.out.println(DiscoveryConstant.N_D_VERSION_FAILOVER + "=" + routeVersionFailover);
+            stringBuilder.append(DiscoveryConstant.N_D_VERSION_FAILOVER + "=" + routeVersionFailover).append("\n");
         }
         String routeRegionTransfer = strategyContextHolder.getHeader(DiscoveryConstant.N_D_REGION_TRANSFER);
         if (StringUtils.isNotEmpty(routeRegionTransfer)) {
-            System.out.println(DiscoveryConstant.N_D_REGION_TRANSFER + "=" + routeRegionTransfer);
+            stringBuilder.append(DiscoveryConstant.N_D_REGION_TRANSFER + "=" + routeRegionTransfer).append("\n");
         }
         String routeRegionFailover = strategyContextHolder.getHeader(DiscoveryConstant.N_D_REGION_FAILOVER);
         if (StringUtils.isNotEmpty(routeRegionFailover)) {
-            System.out.println(DiscoveryConstant.N_D_REGION_FAILOVER + "=" + routeRegionFailover);
+            stringBuilder.append(DiscoveryConstant.N_D_REGION_FAILOVER + "=" + routeRegionFailover).append("\n");
         }
         String routeEnvironmentFailover = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ENVIRONMENT_FAILOVER);
         if (StringUtils.isNotEmpty(routeEnvironmentFailover)) {
-            System.out.println(DiscoveryConstant.N_D_ENVIRONMENT_FAILOVER + "=" + routeEnvironmentFailover);
+            stringBuilder.append(DiscoveryConstant.N_D_ENVIRONMENT_FAILOVER + "=" + routeEnvironmentFailover).append("\n");
         }
         String routeZoneFailover = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ZONE_FAILOVER);
         if (StringUtils.isNotEmpty(routeZoneFailover)) {
-            System.out.println(DiscoveryConstant.N_D_ZONE_FAILOVER + "=" + routeZoneFailover);
+            stringBuilder.append(DiscoveryConstant.N_D_ZONE_FAILOVER + "=" + routeZoneFailover).append("\n");
         }
         String routeAddressFailover = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ADDRESS_FAILOVER);
         if (StringUtils.isNotEmpty(routeAddressFailover)) {
-            System.out.println(DiscoveryConstant.N_D_ADDRESS_FAILOVER + "=" + routeAddressFailover);
+            stringBuilder.append(DiscoveryConstant.N_D_ADDRESS_FAILOVER + "=" + routeAddressFailover).append("\n");
         }
         String routeIdBlacklist = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ID_BLACKLIST);
         if (StringUtils.isNotEmpty(routeIdBlacklist)) {
-            System.out.println(DiscoveryConstant.N_D_ID_BLACKLIST + "=" + routeIdBlacklist);
+            stringBuilder.append(DiscoveryConstant.N_D_ID_BLACKLIST + "=" + routeIdBlacklist).append("\n");
         }
         String routeAddressBlacklist = strategyContextHolder.getHeader(DiscoveryConstant.N_D_ADDRESS_BLACKLIST);
         if (StringUtils.isNotEmpty(routeAddressBlacklist)) {
-            System.out.println(DiscoveryConstant.N_D_ADDRESS_BLACKLIST + "=" + routeAddressBlacklist);
+            stringBuilder.append(DiscoveryConstant.N_D_ADDRESS_BLACKLIST + "=" + routeAddressBlacklist).append("\n");
         }
 
         List<String> requestHeaderNameList = strategyHeaderContext.getRequestHeaderNameList();
@@ -200,10 +206,11 @@ public class DefaultStrategyLogger implements StrategyLogger {
             for (String requestHeaderName : requestHeaderNameList) {
                 String requestHeaderValue = strategyContextHolder.getHeader(requestHeaderName);
                 if (StringUtils.isNotEmpty(requestHeaderValue)) {
-                    System.out.println(requestHeaderName + "=" + requestHeaderValue);
+                    stringBuilder.append(requestHeaderName + "=" + requestHeaderValue).append("\n");
                 }
             }
         }
-        System.out.println("------------------------------------------------------------");
+        stringBuilder.append("------------------------------------------------------------");
+        LOG.info(stringBuilder.toString());
     }
 }
