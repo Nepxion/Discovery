67e01df300eecf8e5c7170f3a39a954c8da4f0f0
HaojunRen <1394997@qq.com>
2022-12-06 19:51:11 +0800

    Add MdcColorConverter

--- discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/logback/LevelColorConverter.java
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/logback/LevelColorConverter.java
@@ -11,7 +11,6 @@ package com.nepxion.discovery.common.logback;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.pattern.color.ANSIConstants;
 import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
 
 // Refer to color definition with https://logback.qos.ch/manual/layouts.html#coloring
@@ -23,19 +22,19 @@ public class LevelColorConverter extends ForegroundCompositeConverterBase<ILoggi
         switch (level.toInt()) {
             // ERROR等级为红色
             case Level.ERROR_INT:
-                return ANSIConstants.RED_FG;
+                return LogbackConstant.RED_FG;
             // WARN等级为黄色
             case Level.WARN_INT:
-                return ANSIConstants.YELLOW_FG;
+                return LogbackConstant.YELLOW_FG;
             // INFO等级为蓝色
             case Level.INFO_INT:
-                return ANSIConstants.GREEN_FG;
+                return LogbackConstant.GREEN_FG;
             // DEBUG等级为绿色
             case Level.DEBUG_INT:
-                return ANSIConstants.BLUE_FG;
+                return LogbackConstant.BLUE_FG;
             // 其他为默认颜色
             default:
-                return ANSIConstants.DEFAULT_FG;
+                return LogbackConstant.DEFAULT_FG;
         }
     }
 }
---
+++ discovery-commons/discovery-common/src/main/java/com/nepxion/discovery/common/logback/LogbackConstant.java
@@ -0,0 +1,16 @@
+package com.nepxion.discovery.common.logback;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import ch.qos.logback.core.pattern.color.ANSIConstants;
+
+public class LogbackConstant extends ANSIConstants {
+    public static final String ANSI_COLOR = "ansi-color";
+}
