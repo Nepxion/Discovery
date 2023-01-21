8fa96860cff685abaf65aad19e1bea42a98b742e
HaojunRen <1394997@qq.com>
2023-01-21 11:13:33 +0800

    修改打包脚本

---
+++ install.bat
@@ -0,0 +1,20 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                     Nepxion Discovery                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Discovery
+@color 0a
+
+call mvn clean install -DskipTests
+
+pause
