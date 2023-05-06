30e1e80b14c932f5e6ed631fde0ca00515e000a6
shenxie <shenxie@msitong.com>
2023-05-06 15:05:19 +0800

    创建agent

---
+++ discovery-agent/src/main/java/com/nepxion/discovery/agent/AgentStarter.java
@@ -0,0 +1,11 @@
+package com.nepxion.discovery.agent;
+
+/**
+ * @author shenxie
+ * @date 2023/5/6
+ */
+public class AgentStarter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
